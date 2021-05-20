768a6d8130eee215f595d5be316daab697db222a
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-05-20 20:35:56 +0100

    Update role.towerdrainer.js

--- role.towerdrainer.js
+++ role.towerdrainer.js
@@ -23,7 +23,57 @@ roles.towerdrainer.settings = {
 };
 
 roles.towerdrainer.getRestPosition = function(creep) {
-  const restRoom = creep.memory.routing.targetRoom;
+  
+  if (!creep.memory.healRoom1) {
+            creep.travelTo(Game.flags['healRoom1']);
+            if (creep.pos.isNearTo(Game.flags['healRoom1'])) {
+                creep.memory.healRoom1 = true;
+            }
+            return;
+        }
+console.log('towerdrainer arrived at healRoom1...')
+  
+   if (!creep.memory.healRoom2) {
+            creep.travelTo(Game.flags['healRoom2']);
+            if (creep.pos.isNearTo(Game.flags['healRoom2'])) {
+                creep.memory.healRoom2 = true;
+            }
+            return;
+        }
+console.log('towerdrainer arrived at healRoom2...')
+  
+   if (!creep.memory.healRoom3) {
+            creep.travelTo(Game.flags['healRoom3']);
+            if (creep.pos.isNearTo(Game.flags['healRoom3'])) {
+                creep.memory.healRoom3 = true;
+            }
+            return;
+        }
+console.log('towerdrainer arrived at healRoom3...')
+  
+  if (!creep.memory.healRoom4) {
+            creep.travelTo(Game.flags['healRoom4']);
+            if (creep.pos.isNearTo(Game.flags['healRoom4'])) {
+                creep.memory.healRoom4 = true;
+            }
+            return;
+        }
+console.log('towerdrainer arrived at healRoom4...')
+  
+   if (!creep.memory.healRoom5) {
+            creep.travelTo(Game.flags['healRoom5']);
+            if (creep.pos.isNearTo(Game.flags['healRoom5'])) {
+                creep.memory.healRoom5 = true;
+            }
+            return;
+        }
+console.log('towerdrainer arrived at healRoom5...')
+  
+  const restRoom1 = creep.memory.healRoom1;  
+  const restRoom2 = creep.memory.healRoom2;
+  const restRoom3 = creep.memory.healRoom3;
+  const restRoom4 = creep.memory.healRoom4;
+  const restRoom5 = creep.memory.healRoom5;
   const attackRoom = creep.memory.attackRoom;
   if (!creep.memory.restPosition) {
     creep.notifyWhenAttacked(false);
