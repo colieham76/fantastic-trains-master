d64fa22bd9586636c3e70ce626a47fb7fa08c6f4
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-05-21 14:03:41 +0100

    Update role.towerdrainer.js

--- role.towerdrainer.js
+++ role.towerdrainer.js
@@ -15,7 +15,7 @@ require('myFunctions');
  * @example
  * Game.rooms.E17N1.memory.queue.push({role: 'towerdrainer', routing: {targetRoom: 'E16N0'}, attackRoom: 'E16N1'})
  */
-
+/*
 roles.towerdrainer = {};
 
 roles.towerdrainer.settings = {
@@ -23,9 +23,9 @@ roles.towerdrainer.settings = {
   amount: [3, 5, 2], // attack RCL 5
   // amount: [2, 3, 1], // attack RCL 3
   maxLayoutAmount: 1,
-};
+};*/
 
-roles.towerdrainer.getRestPosition = function(creep) {
+roles.towerdrainer.getRestPosition = function(creep) {// remove this?
   
   if (!creep.memory.healRoom1) {
             creep.travelTo(Game.flags['healRoom1']);
@@ -80,9 +80,9 @@ console.log('towerdrainer arrived at healRoom5...')
   const attackRoom = creep.memory.attackRoom;
   if (!creep.memory.restPosition) {
     creep.notifyWhenAttacked(false);
-    const room = Game.rooms[restRoom];
+    const room = Game.rooms[restRoom1];
     const attackDirection = room.findExitTo(attackRoom);
-    const restDirection = RoomPosition.oppositeDirection(attackDirection);
+    const restDirection = RoomPosition.oppositeDirection(attackDirection);// remove/rewrite this?
     const occupiedPositions = {};
     _.filter(Game.creeps, (c) => c.memory.role === 'towerdrainer' && c.memory.restPosition).forEach((c) => {
       occupiedPositions[c.memory.restPosition.x + c.memory.restPosition.y] = c.id;
@@ -92,8 +92,8 @@ console.log('towerdrainer arrived at healRoom5...')
       const pos = exit.getAdjacentPosition(restDirection);
       if (!pos.checkForWall() && !pos.checkForObstacleStructure() && !occupiedPositions[pos.x + pos.y]) {
         creep.memory.restPosition = pos;
-        creep.memory.attackDirrection = attackDirection;
-        creep.memory.restDirrection = restDirection;
+        creep.memory.attackDirrection = attackDirection;// remove/rewrite this?
+        creep.memory.restDirrection = restDirection;// remove/rewrite this?
         break;
       }
     }
@@ -101,9 +101,9 @@ console.log('towerdrainer arrived at healRoom5...')
   return creep.memory.restPosition;
 };
 
-roles.towerdrainer.action = function(creep) {
+roles.towerdrainer.action = function(creep) {// remove/rewrite this?
   const attackRoom = creep.memory.attackRoom;
-  const restPos = roles.towerdrainer.getRestPosition(creep);
+  const restPos = roles.towerdrainer.getRestPosition(creep);// remove/rewrite this?
   if (!restPos) {
     creep.log('no position');
     creep.moveRandom();
