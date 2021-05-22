4a4591052cc3f79a67e33eafab94f693babca9f4
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-05-22 21:12:14 +0100

    Update role.longDistanceHarvester.js

--- role.longDistanceHarvester.js
+++ role.longDistanceHarvester.js
@@ -25,7 +25,7 @@ module.exports = {
                     // a property called filter which can be a function
                     // we use the arrow operator to define it
                     filter: (s) => (s.structureType === STRUCTURE_SPAWN
-                            && s.structureType === STRUCTURE_EXTENSION
+                            || s.structureType === STRUCTURE_EXTENSION
                             || s.structureType === STRUCTURE_LINK
                             || s.structureType === STRUCTURE_STORAGE
                             || s.structureType === STRUCTURE_TOWER
