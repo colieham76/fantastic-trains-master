f3ec56c38605bca829dced9377b55a5d35af691f
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-05-23 10:34:34 +0100

    Update role.longDistanceHarvester.js

--- role.longDistanceHarvester.js
+++ role.longDistanceHarvester.js
@@ -1,16 +1,15 @@
 module.exports = {
     // a function to run the logic for this role
     /** @param {Creep} creep */
-    run: function(creep) {
-	    
-	    var terminalVar = creep.room.terminal;
-            var storageVar = creep.room.storage;
-	    	    
+    run: function (creep) {
+
+        var terminalVar = creep.room.terminal;
+        var storageVar = creep.room.storage;
+
         // if creep is bringing energy to a structure but has no energy left
-        if(creep.memory.working && creep.store.energy == 0) {
+        if (creep.memory.working && creep.store.energy == 0) {
             creep.memory.working = false;
-        }
-        else if(!creep.memory.working && creep.store.energy == creep.store.getCapacity()){
+        } else if (!creep.memory.working && creep.store.energy == creep.store.getCapacity()) {
             creep.memory.working = true;
         }
 
@@ -32,12 +31,12 @@ module.exports = {
                         )
                         && s.energy < s.energyCapacity * 0.1
                 });
-            
-                    if (_.sum(creep.carry) > 0) {
-                        if (creep.transfer(storageVar, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                            creep.travelTo(storageVar);
-                        }
-		    }
+
+                if (_.sum(creep.carry) > 0) {
+                    if (creep.transfer(storageVar, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                        creep.travelTo(storageVar);
+                    }
+                }
 
                 if (structure == undefined) {
                     if (_.sum(creep.carry) > 0) {
@@ -69,7 +68,6 @@ module.exports = {
             if (creep.room.name == creep.memory.target) {
 
 
-
                 // find source
                 var source = creep.room.find(FIND_SOURCES)[creep.memory.sourceIndex];
 
@@ -78,70 +76,74 @@ module.exports = {
                     // move towards the source
                     creep.travelTo(source);
                 }
-               
-                                let invaderStructure = creep.pos.findClosestByRange(FIND_HOSTILE_STRUCTURES);
-                                if(invaderStructure){
-                                    if (creep.room.name === 'W3S7' && Game.time % 20 === 0) {
-                                        Game.spawns.Spawn3.memory.rangedattackerRoom = 'W3S7';
-                                       // Game.spawns.Spawn21.memory.rangedattackerRoom = 'W64N63'
-                                    }
-				}
-				    
-				    /*
-                                    if (creep.room.name === 'W78N64' && Game.time % 500 === 0) {
-                                        Game.spawns.Spawn20.memory.rangedattackerRoom = 'W78N64';
-                                        Game.spawns.Spawn22.memory.rangedattackerRoom = 'W78N64'
-                                    }
-                                    if (creep.room.name === 'W62N67' && Game.time % 500 === 0) {
-                                        Game.spawns.Spawn16.memory.rangedattackerRoom = 'W62N67';
-                                        Game.spawns.Spawn6.memory.rangedattackerRoom = 'W62N67'
-                                    }
-                                    if (creep.room.name === 'W63N68' && Game.time % 510 === 0) {
-                                        Game.spawns.Spawn16.memory.rangedattackerRoom = 'W63N68';
-                                        Game.spawns.Spawn6.memory.rangedattackerRoom = 'W63N68'
-                                    }
-                                }*/
+
+                let invaderStructure = creep.pos.findClosestByRange(FIND_HOSTILE_STRUCTURES);
+                if (invaderStructure) {
+                    if (creep.room.name === 'W3S7' && Game.time % 20 === 0) {
+                        Game.spawns.Spawn3.memory.rangedattackerRoom = 'W3S7';
+                        // Game.spawns.Spawn21.memory.rangedattackerRoom = 'W64N63'
+                    }
+                }
+                /*
+
+                     if (creep.room.name === 'W62N67' && Game.time % 500 === 0) {
+                         Game.spawns.Spawn16.memory.rangedattackerRoom = 'W62N67';
+                         Game.spawns.Spawn6.memory.rangedattackerRoom = 'W62N67'
+                     }
+                     if (creep.room.name === 'W63N68' && Game.time % 510 === 0) {
+                         Game.spawns.Spawn16.memory.rangedattackerRoom = 'W63N68';
+                         Game.spawns.Spawn6.memory.rangedattackerRoom = 'W63N68'
+                     }
+                 }*/
 
 
-                /*           let invaderCreep = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-                           if(invaderCreep){
-                               if (creep.room.name === 'W7S9' && Game.time % 20 === 0) {
-                                   Game.spawns.Spawn1.memory.rangedattackerRoom = 'W7S9';
-                                  // Game.spawns.Spawn21.memory.rangedattackerRoom = 'W64N63'
-                               }
-                               if (creep.room.name === 'W8S8' && Game.time % 30 === 0) {
-                                   Game.spawns.Spawn1.memory.rangedattackerRoom = 'W8S8';
-                                //   Game.spawns.Spawn22.memory.rangedattackerRoom = 'W78N64'
-                               }
-                               if (creep.room.name === 'W7S8' && Game.time % 50 === 0) {
-                                   Game.spawns.Spawn1.memory.rangedattackerRoom = 'W7S8';
-                                 //  Game.spawns.Spawn6.memory.rangedattackerRoom = 'W62N67'
-                               }
-                               if (creep.room.name === 'W9S8' && Game.time % 40 === 0) {
-                                   Game.spawns.Spawn1.memory.rangedattackerRoom = 'W9S8';
-                                 //  Game.spawns.Spawn6.memory.rangedattackerRoom = 'W63N68'
-                               }
-                           }*/
+                let invaderCreep = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+                if (invaderCreep) {
+                    if (creep.room.name === 'W7S9' && Game.time % 20 === 0) {
+                        Game.spawns.Spawn1.memory.rangedattackerRoom = 'W7S9';
+                        // Game.spawns.Spawn21.memory.rangedattackerRoom = 'W64N63'
+                    }
+                    if (creep.room.name === 'W8S8' && Game.time % 15 === 0) {
+                        Game.spawns.Spawn1.memory.rangedattackerRoom = 'W8S8';
+                        //   Game.spawns.Spawn22.memory.rangedattackerRoom = 'W78N64'
+                    }
+
+                    if (creep.room.name === 'W1S9' && Game.time % 15 === 0) {
+                        Game.spawns.Spawn5.memory.rangedattackerRoom = 'W1S9';
+                        //   Game.spawns.Spawn22.memory.rangedattackerRoom = 'W78N64'
+                    }                     
+                }
+                    /*
+                    if (creep.room.name === 'W7S8' && Game.time % 50 === 0) {
+                        Game.spawns.Spawn1.memory.rangedattackerRoom = 'W7S8';
+                      //  Game.spawns.Spawn6.memory.rangedattackerRoom = 'W62N67'
+                    }
+                    if (creep.room.name === 'W9S8' && Game.time % 40 === 0) {
+                        Game.spawns.Spawn1.memory.rangedattackerRoom = 'W9S8';
+                      //  Game.spawns.Spawn6.memory.rangedattackerRoom = 'W63N68'
+                    }
+                }*/
+
+                // if not in target room
+                else {
+                    // find exit to target room
+                    var exit = creep.room.findExitTo(creep.memory.target);
+                    // move to exit
+                    creep.travelTo(creep.pos.findClosestByRange(exit));
+                }
             }
-            // if not in target room
-            else {
-                // find exit to target room
-                var exit = creep.room.findExitTo(creep.memory.target);
-                // move to exit
-                creep.travelTo(creep.pos.findClosestByRange(exit));
+
+            // pickup dropped energy in target room
+            var dropedEnergy = creep.pos.findClosestByRange(FIND_DROPPED_RESOURCES, {
+                filter: (e) => (e.resourceType == RESOURCE_ENERGY) && e.energy > 250
+            });
+            if (dropedEnergy) {
+                if (creep.pickup(dropedEnergy) === ERR_NOT_IN_RANGE) {
+                    creep.travelTo(dropedEnergy, {visualizePathStyle: {stroke: '#e70808'}});
+                    creep.say('Kprs');
+                }
             }
+
         }
-
-        // pickup dropped energy in target room
-        var dropedEnergy = creep.pos.findClosestByRange(FIND_DROPPED_RESOURCES, {
-            filter: (e) => (e.resourceType == RESOURCE_ENERGY) && e.energy > 250
-        });
-        if (dropedEnergy) {
-            if (creep.pickup(dropedEnergy) === ERR_NOT_IN_RANGE) {
-                creep.travelTo(dropedEnergy, {visualizePathStyle: {stroke: '#e70808'}});
-                creep.say('Kprs');
-            }
-        }
-
     }
-};
+}
