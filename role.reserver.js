cb5b10a9dc63c12b3720e1662aeb7c5593e27877
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-05-24 11:50:27 +0100

    Update role.reserver.js

--- role.reserver.js
+++ role.reserver.js
@@ -1,25 +1,25 @@
 module.exports = {
 	run: function(creep) {
 		if (creep.room.name != creep.memory.target) {
-			if (!creep.memory.W3S7 && creep.room.name === 'W3S7'|| creep.room.name === 'W3S8') {
+			//if (!creep.memory.W3S7 && creep.room.name === 'W3S7'|| creep.room.name === 'W3S8') {
 				creep.moveTo(Game.flags['ReserverW3S7']);
 				if (creep.pos.isNearTo(Game.flags['ReserverW3S7'])) {
 					creep.memory.W3S7 = true;
 				}
 				return;
-			}
+		//	}
 			else {
 				if (creep.reserveController(creep.room.controller) == ERR_NOT_IN_RANGE) {
 					creep.travelTo(creep.room.controller);
 				}
 			}
-			if (!creep.memory.W1S7 && creep.room.name === 'W1S7'|| creep.room.name === 'W1S8') {
+		//	if (!creep.memory.W1S7 && creep.room.name === 'W1S7'|| creep.room.name === 'W1S8') {
 				creep.moveTo(Game.flags['ReserverW1S7']);
 				if (creep.pos.isNearTo(Game.flags['ReserverW1S7'])) {
 					creep.memory.W1S7 = true;
 				}
 				return;
-			}
+		//	}
 			else {
 				if (creep.reserveController(creep.room.controller) == ERR_NOT_IN_RANGE) {
 					creep.travelTo(creep.room.controller);
