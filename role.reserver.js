8bdebba7237bccb7319ae6211bad6fc166d0a076
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-05-24 16:14:17 +0100

    Update role.reserver.js

--- role.reserver.js
+++ role.reserver.js
@@ -1,66 +1,66 @@
 module.exports = {
 	run: function(creep) {
 		if (creep.room.name != creep.memory.target) {
-			//if (!creep.memory.W3S7 && creep.room.name === 'W3S7'|| creep.room.name === 'W3S8') {
+			if (!creep.memory.W3S7 && creep.room.name === 'W3S7'|| creep.room.name === 'W3S8') {
 				creep.moveTo(Game.flags['ReserverW3S7']);
 				if (creep.pos.isNearTo(Game.flags['ReserverW3S7'])) {
-				//	creep.memory.W3S7 = true;
+					creep.memory.W3S7 = true;
 				}
 				return;
-		//	}
-		//	else {
+			}
+			else {
 				if (creep.reserveController(creep.room.controller) == ERR_NOT_IN_RANGE) {
 					creep.moveTo(creep.room.controller);
 				}
-		//	}
-		//	if (!creep.memory.W1S7 && creep.room.name === 'W1S7'|| creep.room.name === 'W1S8') {
+			}
+			if (!creep.memory.W1S7 && creep.room.name === 'W1S7'|| creep.room.name === 'W1S8') {
 				creep.moveTo(Game.flags['ReserverW1S7']);
 				if (creep.pos.isNearTo(Game.flags['ReserverW1S7'])) {
-				//	creep.memory.W1S7 = true;
+					creep.memory.W1S7 = true;
 				}
 				return;
-		//	}
-		//	else {
+			}
+			else {
 				if (creep.reserveController(creep.room.controller) == ERR_NOT_IN_RANGE) {
 					creep.moveTo(creep.room.controller);
 				}				
-		//	}
-		//	if (!creep.memory.W1S9 && creep.room.name === 'W1S9'|| creep.room.name === 'W1S8') {
+			}
+			if (!creep.memory.W1S9 && creep.room.name === 'W1S9'|| creep.room.name === 'W1S8') {
 				creep.moveTo(Game.flags['ReserverW1S9']);
 				if (creep.pos.isNearTo(Game.flags['ReserverW1S9'])) {
-				//	creep.memory.W1S9 = true;
+					creep.memory.W1S9 = true;
 				}
 				return;
-		//	}
-		//	else {
+			}
+			else {
 				 if (creep.reserveController(creep.room.controller) == ERR_NOT_IN_RANGE) {
 					 creep.moveTo(creep.room.controller);
 				 }
-		//	 }
-		//	if (!creep.memory.W8S8 && creep.room.name === 'W8S8'|| creep.room.name === 'W7S8') {
+			 }
+			if (!creep.memory.W8S8 && creep.room.name === 'W8S8'|| creep.room.name === 'W7S8') {
 				creep.moveTo(Game.flags['ReserverW8S8']);
 				if (creep.pos.isNearTo(Game.flags['ReserverW8S8'])) {
-				//	creep.memory.W8S8 = true;
+					creep.memory.W8S8 = true;
 				}
 				return;
-		//	}
-		//	 else {
+			}
+			 else {
 				 if (creep.reserveController(creep.room.controller) == ERR_NOT_IN_RANGE) {
 					 creep.moveTo(creep.room.controller);
 				 }
-		//	 }
-		//	 if (!creep.memory.W7S9 && creep.room.name === 'W7S9'|| creep.room.name === 'W7S8') {
+			 }
+			 if (!creep.memory.W7S9 && creep.room.name === 'W7S9'|| creep.room.name === 'W7S8') {
 			 	creep.moveTo(Game.flags['ReserverW7S9']);
 			 	if (creep.pos.isNearTo(Game.flags['ReserverW7S9'])) {
-			 	//	creep.memory.W7S9 = true;
+			 		creep.memory.W7S9 = true;
 			 	}
 			 	return;
-		//	 }
-		//	 else {
+			 }
+			 else {
 			 	if (creep.reserveController(creep.room.controller) == ERR_NOT_IN_RANGE) {
 			 		creep.moveTo(creep.room.controller);
 			 	}
-		//	 }
+			 }
 		}
 	}
 };
