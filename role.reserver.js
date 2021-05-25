41b20b8d7d6a7f5130f6efe3c3ded0b07b320943
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-05-25 04:38:14 +0100

    Update role.reserver.js

--- role.reserver.js
+++ role.reserver.js
@@ -1,6 +1,6 @@
 module.exports = {
 	run: function(creep) {		
-			if (!creep.memory.W3S7 && creep.room.name === 'W3S8'|| creep.room.name === 'W3S7') {
+			if (!creep.memory.W3S7 && creep.room.name === 'W3S7'|| creep.room.name === 'W3S8') {
 				creep.travelTo(Game.flags['ReserverW3S7']);
 				if (creep.pos.isNearTo(Game.flags['ReserverW3S7'])) {
 					creep.memory.W3S7 = true;
