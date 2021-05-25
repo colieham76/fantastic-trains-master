32c07c2ea34d295f4371f6a05d517e15a0902b75
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-05-25 02:59:42 +0100

    Update role.reserver.js

--- role.reserver.js
+++ role.reserver.js
@@ -1,58 +1,58 @@
 module.exports = {
 	run: function(creep) {		
 			if (!creep.memory.W3S7 && creep.room.name === 'W3S8'|| creep.room.name === 'W3S7') {
-				creep.moveTo(Game.flags['ReserverW3S7']);
+				creep.travelTo(Game.flags['ReserverW3S7']);
 				if (creep.pos.isNearTo(Game.flags['ReserverW3S7'])) {
 					creep.memory.W3S7 = true;
 				}				
 			}
 			else {
 				if (creep.reserveController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-					creep.moveTo(creep.room.controller);
+					creep.travelTo(creep.room.controller);
 				}
 			}
 			if (!creep.memory.W1S7 && creep.room.name === 'W1S7'|| creep.room.name === 'W1S8') {
-				creep.moveTo(Game.flags['ReserverW1S7']);
+				creep.travelTo(Game.flags['ReserverW1S7']);
 				if (creep.pos.isNearTo(Game.flags['ReserverW1S7'])) {
 					creep.memory.W1S7 = true;
 				}				
 			}
 			else {
 				if (creep.reserveController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-					creep.moveTo(creep.room.controller);
+					creep.travelTo(creep.room.controller);
 				}				
 			}
 			if (!creep.memory.W1S9 && creep.room.name === 'W1S9'|| creep.room.name === 'W1S8') {
-				creep.moveTo(Game.flags['ReserverW1S9']);
+				creep.travelTo(Game.flags['ReserverW1S9']);
 				if (creep.pos.isNearTo(Game.flags['ReserverW1S9'])) {
 					creep.memory.W1S9 = true;
 				}				
 			}
 			else {
 				 if (creep.reserveController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-					 creep.moveTo(creep.room.controller);
+					 creep.travelTo(creep.room.controller);
 				 }
 			 }
 			if (!creep.memory.W8S8 && creep.room.name === 'W8S8'|| creep.room.name === 'W7S8') {
-				creep.moveTo(Game.flags['ReserverW8S8']);
+				creep.travelTo(Game.flags['ReserverW8S8']);
 				if (creep.pos.isNearTo(Game.flags['ReserverW8S8'])) {
 					creep.memory.W8S8 = true;
 				}				
 			}
 			 else {
 				 if (creep.reserveController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-					 creep.moveTo(creep.room.controller);
+					 creep.travelTo(creep.room.controller);
 				 }
 			 }
 			 if (!creep.memory.W7S9 && creep.room.name === 'W7S9'|| creep.room.name === 'W7S8') {
-			 	creep.moveTo(Game.flags['ReserverW7S9']);
+			 	creep.travelTo(Game.flags['ReserverW7S9']);
 			 	if (creep.pos.isNearTo(Game.flags['ReserverW7S9'])) {
 			 		creep.memory.W7S9 = true;
 			 	}			 	
 			 }
 			 else {
 			 	if (creep.reserveController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-			 		creep.moveTo(creep.room.controller);
+			 		creep.travelTo(creep.room.controller);
 			 	}
 			 }
 	}
