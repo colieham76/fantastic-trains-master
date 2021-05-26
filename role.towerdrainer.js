855c0e74f26b65ad7b2f9b08d0ddeb6d9bee3924
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-05-26 12:10:48 +0100

    Update role.towerdrainer.js

--- role.towerdrainer.js
+++ role.towerdrainer.js
@@ -68,3 +68,40 @@ roles.towerdrainer.action = function(creep) {
 
   return true;
 };
+
+
+///////////////////////
+/*
+var roleUpgrader = require('role.upgrader');
+
+module.exports = {
+    build: function(spawn, size) {
+        spawn.spawnCreep(
+            [WORK, WORK, CARRY, MOVE, MOVE, MOVE],
+            'Drone' + Math.floor(Math.random() * 10000),
+            { memory: { role: 'drone', rally: 'work_1', traveling: true } }
+        )
+    },
+    
+    run: function(creep) {
+        if(creep.memory.rally) {
+            var flag = Game.flags['work_1'];
+            
+            if(creep.memory.traveling) {
+                if(creep.pos.getRangeTo(flag) > 1) {
+                    creep.moveTo(flag);
+                } else {
+                    creep.memory.traveling = false;
+                }
+            } else {
+                roleUpgrader.run(creep, creep.room.controller);
+            }
+        }
+    }
+};
+*/
+
+
+
+
+
