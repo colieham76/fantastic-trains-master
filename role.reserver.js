875d720b6227960decb9db1e0f20eab526a61770
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-05-27 19:24:23 +0100

    Update role.reserver.js

--- role.reserver.js
+++ role.reserver.js
@@ -11,11 +11,10 @@ module.exports = {
 				if (creep.room.controller && !creep.room.controller.my) {
 					if (creep.attackController(creep.room.controller) == ERR_NOT_IN_RANGE) {
 						creep.moveTo(creep.room.controller);
-					}
-				}
-				else {
-					if (creep.reserveController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-						creep.travelTo(creep.room.controller);
+					} else if (creep.memory.W3S7 == true) {
+						if (creep.reserveController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+							creep.moveTo(creep.room.controller);
+						}
 					}
 				}
 			}
