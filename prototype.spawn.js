2bf80546abdc71ba3394c6e0ddf7c973e2331d51
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-05-28 12:39:35 +0100

    Update prototype.spawn.js

--- prototype.spawn.js
+++ prototype.spawn.js
@@ -32,11 +32,11 @@ Game.spawns.Spawn1.memory.minLongDistanceHarvesters = {W7S9: 2};
 Game.spawns.W7S8.memory.minCreeps = {harvester: 0, lorry: 1};
 Game.spawns.W7S8.memory.minLongDistanceHarvesters = {W8S8: 2};
 
-Game.spawns.Spawn2.memory.minCreeps = {repairer: 0, builder: 0, lorry: 2, towerlorry: 2, upgrader: 0, harvester: 0,
+Game.spawns.Spawn2.memory.minCreeps = {repairer: 0, builder: 0, lorry: 0, towerlorry: 2, upgrader: 0, harvester: 1,
                                         wallRepairer: 0, rampartrepairer: 0, harvester: 0};
-Game.spawns.Spawn5.memory.minCreeps = {lorry: 1, upgrader: 1};
-Game.spawns.Spawn2.memory.minLongDistanceHarvesters = {W1S9: 2};
-Game.spawns.Spawn5.memory.minLongDistanceHarvesters = {W1S7: 2}
+Game.spawns.Spawn5.memory.minCreeps = {lorry: 1, upgrader: 0};
+Game.spawns.Spawn2.memory.minLongDistanceHarvesters = {W1S9: 1};
+Game.spawns.Spawn5.memory.minLongDistanceHarvesters = {W1S7: 1}
 
 
 Game.spawns.Spawn3.memory.minCreeps = {harvester: 0, repairer: 0, builder: 0, lorry: 2, wallRepairer: 0,
