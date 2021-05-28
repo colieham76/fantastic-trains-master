49c87b93655b45f780bd7d92cb4d49a54156bdbc
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-05-28 17:09:12 +0100

    Update role.towerdrainer5.js

--- role.towerdrainer5.js
+++ role.towerdrainer5.js
@@ -1,1 +1,15 @@
 
+module.exports = {
+
+    run: function (creep) {
+
+if(!creep.memory.rally5) {
+            creep.travelTo(Game.flags['rally5']);
+
+            if (creep.pos.isNearTo(Game.flags['rally5'])) {
+                creep.memory.rally5 = true;
+            }
+            return;
+        }
+    }
+}
