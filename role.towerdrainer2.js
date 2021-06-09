38031b9c8a3fc4ac51654e8560eae983a7cff5c3
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-06-09 11:36:33 +0100

    Update role.towerdrainer2.js

--- role.towerdrainer2.js
+++ role.towerdrainer2.js
@@ -1,16 +1,56 @@
-
+'use strict';
+var selfRenew = require('action.selfRenew');
 module.exports = {
 
     run: function (creep) {
 
-if(!creep.memory.rally2) {
-            creep.travelTo(Game.flags['rally2']);
+        if (creep.ticksToLive < 300) {
+            creep.memory.recycled = false;
 
-            if (creep.pos.isNearTo(Game.flags['rally2'])) {
-                creep.memory.rally2 = true;
+            creep.travelTo(Game.flags['waypoint1']);
+            if (creep.pos.isNearTo(Game.flags['waypoint1'])) {
+                creep.memory.waypoint1 = true;
+            }
+            creep.say('need renew');
+            selfRenew.run(creep);
+        }
+        else if(creep.ticksToLive < 1450){
+            selfRenew.run(creep);
+        }
+        else if (creep.ticksToLive > 1400) {
+            creep.memory.recycled = true;
+            creep.memory.waypoint1 = false;
+        }
+        if (!creep.memory.waypoint1 && (creep.memory.recycled = true)) {
+            creep.travelTo(Game.flags['waypoint1']);
+            if (creep.pos.isNearTo(Game.flags['waypoint1'])) {
+                creep.memory.waypoint1 = true;
             }
             return;
         }
+        else
+        if (!creep.memory.rally2 && (creep.memory.recycled = true)) {
+            creep.travelTo(Game.flags['rally2']);
+
+            if (creep.pos.isNearTo(Game.flags['rally2'])) {
+                creep.memory.rally1 = true;
+                // creep.memory.waypoint1 = false;
+            }
+            return;
+        }
+        if (creep.memory.recycled) {
+            creep.notifyWhenAttacked(false);
+
+            if (creep.hits > 0.95 * creep.hitsMax) { // if full health
+                creep.travelTo(Game.flags['attack2']);
+            } else if (creep.hits < 0.6 * creep.hitsMax) { // if full health
+                creep.travelTo(Game.flags['rally2']);
+            }
+
+            if (!creep.memory.healingAbility) {
+                creep.memory.healingAbility = healingability(creep);
+            }
+            creep.heal(creep);
+        }
     }
 }
-        
