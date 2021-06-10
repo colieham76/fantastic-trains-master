e2991627ee82778443b59a93fd30899933117f2c
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-06-10 13:28:31 +0100

    Update role.wallRepairer.js

--- role.wallRepairer.js
+++ role.wallRepairer.js
@@ -4,17 +4,14 @@ module.exports = {
     // a function to run the logic for this role
     /** @param {Creep} creep */
     run: function(creep) {
-        // if creep is trying to repair something but has no energy left
-        if (creep.memory.working == true && creep.carry.energy == 0) {
-            // switch state
+        if (creep.memory.working && creep.store[RESOURCE_ENERGY] === 0) {
             creep.memory.working = false;
+            creep.say('🔄 harvest');
         }
-        // if creep is harvesting energy but is full
-        else if (creep.memory.working == false && creep.carry.energy == creep.carryCapacity) {
-            // switch state
+        if (!creep.memory.working && creep.store.getFreeCapacity() === 0) {
             creep.memory.working = true;
+            creep.say('offload');
         }
-
         // if creep is supposed to repair something
         if (creep.memory.working == true) {
             // find all walls in the room
@@ -25,7 +22,7 @@ module.exports = {
             var target = undefined;
 
             // loop with increasing percentages
-            for (let percentage = 0.00001; percentage <= 1; percentage = percentage + 0.00001){
+            for (let percentage = 0.00001; percentage <= 1; percentage = percentage + 0.0001){
                 // find a wall with less than percentage hits
                 for (let wall of walls) {
                     if (wall.hits / wall.hitsMax < percentage) {
@@ -33,14 +30,12 @@ module.exports = {
                         break;
                     }
                 }
-
                 // if there is one
                 if (target != undefined) {
                     // break the loop
                     break;
                 }
             }
-
             // if we find a wall that has to be repaired
             if (target != undefined) {
                 // try to repair it, if not in range
