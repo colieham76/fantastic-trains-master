f0729f5b069f2667054050c86691cb6e7eb71163
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-06-10 23:06:24 +0100

    Update prototype.spawn.js

--- prototype.spawn.js
+++ prototype.spawn.js
@@ -64,10 +64,10 @@ Game.spawns.Spawn5.memory.mintowerdrainers5 = {W1S8: 0}
 
 /*
 Game.spawns.Spawn5.memory.mintowerdrainers1 = {W1S8: 1}
-*/
+
 
 Game.spawns.Spawn5.memory.mintowerdrainers2 = {W1S8: 1}
-/*
+
 Game.spawns.Spawn5.memory.mintowerdrainers3 = {W1S8: 1}
 
 Game.spawns.Spawn5.memory.mintowerdrainers4 = {W1S8: 1}
