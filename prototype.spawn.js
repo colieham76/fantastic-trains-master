b8c253661db8117c0e4519e140c469056fd21e60
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-06-10 12:54:09 +0100

    Update prototype.spawn.js

--- prototype.spawn.js
+++ prototype.spawn.js
@@ -52,8 +52,11 @@ Game.spawns.Spawn3.memory.minLongDistanceHarvesters = {W3S7: 2};
 Game.spawns.Spawn3.memory.minNumberOfreservers = {W3S7: 1}
 Game.spawns.Spawn5.memory.minNumberOfreservers = {W1S7: 1}
 
+Game.spawns.Spawn5.memory.mintowerdrainers1 = {W1S8: 0}
 Game.spawns.Spawn5.memory.mintowerdrainers2 = {W1S8: 0}
-
+Game.spawns.Spawn5.memory.mintowerdrainers3 = {W1S8: 1}
+Game.spawns.Spawn5.memory.mintowerdrainers4 = {W1S8: 0}
+Game.spawns.Spawn5.memory.mintowerdrainers5 = {W1S8: 0}
 
 
 //Game.creeps['Isabelle'].memory.target = 'W79N64'
