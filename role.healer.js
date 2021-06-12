9491156d8684e5fe938bf65956377253103a3189
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-06-12 16:55:45 +0100

    Update role.healer.js

--- role.healer.js
+++ role.healer.js
@@ -28,11 +28,7 @@ module.exports = {
                               
             creep.say('need renew');
                 selfRenew.run(creep);
-        }        
-        else if(creep.ticksToLive < 1450){
-            selfRenew.run(creep);
-        }               
-        
+        }                            
         else if (creep.hits > 0.98*creep.hitsMax) { // if full health
                 creep.travelTo(new RoomPosition(2,20, creep.memory.target));
                 if (creep.room.name == creep.memory.target) {// if creep in target room
