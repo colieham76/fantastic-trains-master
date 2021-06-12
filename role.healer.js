7c5e02e8fe08edb3b96c442b38d668adf5454f73
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-06-12 23:31:16 +0100

    Update role.healer.js

--- role.healer.js
+++ role.healer.js
@@ -48,7 +48,7 @@ module.exports = {
                return;
             }
             else if (creep.hits > 0.98*creep.hitsMax) { // if full health
-                creep.travelTo(new RoomPosition(2,21, creep.memory.target));
+                creep.travelTo(new RoomPosition(1,21, creep.memory.target));
                 if (creep.room.name == creep.memory.target) {// if creep in target room
                     let toHeal = lowestHealthInRoom(creep);
                     if (toHeal.hits!=toHeal.hitsMax&&creep.heal(toHeal)==0) { // if found creep
