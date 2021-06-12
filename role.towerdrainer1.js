76cd6b3151f3bfc196e99683f56164de14094c43
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-06-12 17:02:01 +0100

    Update role.towerdrainer1.js

--- role.towerdrainer1.js
+++ role.towerdrainer1.js
@@ -13,10 +13,7 @@ module.exports = {
             }            
             creep.say('need renew');
                 selfRenew.run(creep);
-        }        
-        else if(creep.ticksToLive < 1450){
-            selfRenew.run(creep);
-        }
+        }              
         else if (creep.ticksToLive > 1400) {
             creep.memory.recycled = true;
             creep.memory.waypoint1 = false;
