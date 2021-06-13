f73ee2cd6fa95deb5b76f81a37610a28cac3663b
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-06-13 04:15:05 +0100

    Update prototype.spawn.js

--- prototype.spawn.js
+++ prototype.spawn.js
@@ -65,7 +65,7 @@ Game.spawns.Spawn5.memory.minNumberOfreservers = {W1S7: 1}
 
 Game.spawns.Spawn5.memory.mintowerdrainers1 = {W1S8: 0}
 
-Game.spawns.Spawn5.memory.mintowerdrainers2 = {W1S8: 1}
+Game.spawns.Spawn5.memory.mintowerdrainers2 = {W1S8: 0}
 
 Game.spawns.Spawn5.memory.mintowerdrainers3 = {W1S8: 0}
 
