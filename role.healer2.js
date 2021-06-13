43da381fc6383dee643a8b72e704bd021ed3da63
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-06-13 15:20:41 +0100

    Update role.healer2.js

--- role.healer2.js
+++ role.healer2.js
@@ -32,16 +32,15 @@ module.exports = {
             }            
             creep.say('need renew');
                 selfRenew.run(creep);
-        }  
-        
+        }          
                 
         else if(creep.ticksToLive < 1450){
             selfRenew.run(creep);
-        }
-        
+        }        
         else if (creep.ticksToLive > 1400) {
             creep.memory.recycled = true;
             creep.memory.waypoint1 = false;
+            creep.memory.attaaaacck = true;
         }
         if (!creep.memory.waypoint1 && (creep.memory.recycled = true) && (creep.memory.attaaaacck = true)) {//enroute
             creep.travelTo(Game.flags['waypoint1']);
@@ -50,34 +49,18 @@ module.exports = {
                 }
                return;
             }
-            else if (creep.hits > 0.98*creep.hitsMax) { // if full health
-                creep.travelTo(new RoomPosition(1,23, creep.memory.target));
-                creep.memory.attaaaacck = true;  
+            if (creep.hits > 0.98*creep.hitsMax) { // if full health
+                creep.travelTo(new RoomPosition(1,21, creep.memory.target));
+                creep.memory.attaaaacck = false;  
                 if (creep.room.name == creep.memory.target) {// if creep in target room
                     let toHeal = lowestHealthInRoom(creep);
                     if (toHeal.hits!=toHeal.hitsMax&&creep.heal(toHeal)==0) { // if found creep
                     }
                     else {
-                        //creep.rangedHeal(toHeal);
-                       creep.heal(toHeal);
+                        creep.rangedHeal(toHeal);
+                       // creep.moveTo(toHeal);
                     }
-                }
-                else {
-                    creep.travelTo(new RoomPosition(3,17, creep.memory.target));
-                    creep.heal(creep);
-                }
+                }               
             }
-            else { // wounded, escape
-                if (Game.flags[creep.memory.target+'esc']) {
-                    creep.moveTo(Game.flags[creep.memory.target+'esc']);
-                    creep.heal(creep);
-                }/*
-                else {
-                    var exit = creep.room.findExitTo(creep.memory.target);
-                    creep.moveTo(creep.pos.findClosestByRange(exit));
-                    creep.heal(creep);
-                }*/
-            }
-      //  }
     }
 };
