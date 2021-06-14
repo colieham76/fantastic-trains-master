935758f35c1a4c85e389c37ccc4c050d6366d7f3
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-06-14 01:25:42 +0100

    Update prototype.spawn.js

--- prototype.spawn.js
+++ prototype.spawn.js
@@ -47,9 +47,9 @@ Game.spawns.Spawn2.memory.minhealers = {W1S8: 1};
 
 Game.spawns.Spawn2.memory.minhealers2 = {W1S8: 1};
 
-Game.spawns.Spawn2.memory.minhealers3 = {W1S8: 1};
+Game.spawns.Spawn2.memory.minhealers3 = {W1S8: 0};
 
-Game.spawns.Spawn2.memory.minhealers4 = {W1S8: 1};
+Game.spawns.Spawn2.memory.minhealers4 = {W1S8: 0};
 
 
 Game.spawns.Spawn2.memory.minsmallUpgraders = {W1S8: 1};
