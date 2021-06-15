fd6542b3789e1386eca4c419d37d3e2dfb93b096
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-06-15 13:38:47 +0100

    Update role.towerdrainer4.js

--- role.towerdrainer4.js
+++ role.towerdrainer4.js
@@ -3,6 +3,8 @@ var selfRenew = require('action.selfRenew');
 module.exports = {
 
     run: function (creep) {
+        var rallypos = new RoomPosition(1,22,'W1S8');
+
 
         if (creep.ticksToLive < 300) {// double back
             creep.memory.recycled = false;
@@ -15,7 +17,8 @@ module.exports = {
             selfRenew.run(creep);
         }
         
-        else if(creep.ticksToLive < 1450){
+         else if (creep.pos.x !== rallypos.x && creep.pos.y !== rallypos.y) {
+            if (creep.ticksToLive < 1450) {
             selfRenew.run(creep);
         }
         
@@ -30,16 +33,7 @@ module.exports = {
             }
             return;
         }
-        else
-        if (!creep.memory.rally4 && (creep.memory.waypoint4 = true) && (creep.memory.recycled = true)) {// enroute
-            creep.travelTo(Game.flags['rally4']);
-
-            if (creep.pos.isNearTo(Game.flags['rally4'])) {
-                creep.memory.rally4 = true;
-                creep.memory.attaaaacck = true;
-            }
-            return;
-        }
+         }
         if (creep.memory.recycled &&  (creep.memory.attaaaacck = true)) {
             creep.notifyWhenAttacked(false);
 
@@ -58,7 +52,9 @@ module.exports = {
             if (!creep.memory.healingAbility) {
                 creep.memory.healingAbility = healingability(creep);
             }
+             if(Game.time % 3 === 0){
             creep.heal(creep);
+             }
         }
     }
 }
