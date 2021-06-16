055b105d0268e7ae3607406d3f02598869edd94a
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-06-16 15:42:13 +0100

    Update role.healer2.js

--- role.healer2.js
+++ role.healer2.js
@@ -32,7 +32,7 @@ module.exports = {
             }
             creep.say('need 2 renew');
 
-               var room = room.name
+               var room = Game.rooms['W1S8']
                 selfRenew.run(creep);
             if (room.energyCapacityAvailable < 1000) {
                 creep.cancelOrder(selfRenew)
