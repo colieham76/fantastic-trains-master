7946bab38851ffd91a489bb6cb933945e6055d1a
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-06-17 07:18:09 +0100

    Update action.load.js

--- action.load.js
+++ action.load.js
@@ -1,7 +1,11 @@
 module.exports = {
     run: function(creep) {
         if (creep.carry.energy>0) {
-            let toFill = creep.pos.findClosestByRange(FIND_MY_STRUCTURES, { filter: (s) => ( (s.structureType == STRUCTURE_SPAWN || s.structureType == STRUCTURE_EXTENSION || ( s.structureType == STRUCTURE_TOWER && (s.energy < s.energyCapacity*0.7) ) || s.structureType == STRUCTURE_LAB) && s.energy < s.energyCapacity) })
+            let toFill = creep.pos.findClosestByRange(FIND_MY_STRUCTURES, { 
+                filter: (s) => ( (s.structureType == STRUCTURE_SPAWN
+                                  || s.structureType == STRUCTURE_EXTENSION
+                                  //|| ( s.structureType == STRUCTURE_TOWER && (s.energy < s.energyCapacity*0.7) ) 
+                                  || s.structureType == STRUCTURE_LAB) && s.energy < s.energyCapacity) })
             if (toFill == undefined) {
                 toFill = creep.room.storage;
             }
