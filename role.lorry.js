3982f7e9e37d2876b73819ead3d8cf79887a9ab0
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-06-18 12:45:19 +0100

    Update role.lorry.js

--- role.lorry.js
+++ role.lorry.js
@@ -27,35 +27,16 @@ module.exports = {
                 }
             }
         }
-        else {
-
-            for (const resourceType in creep.getEnergy()) {
-                if (resourceType == RESOURCE_ENERGY) {
-
-                    var structure = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
-                        filter: (s) => (s.structureType === STRUCTURE_SPAWN
-                            || s.structureType === STRUCTURE_EXTENSION
-                            || s.structureType === STRUCTURE_NUKER)
-                            && s.energy < s.energyCapacity
-                    });
-                    if (structure == undefined) {
-                        structure = creep.room.storage;
-                        if (structure) {
-                            if (structure.store[RESOURCE_ENERGY] > 0.8 * structure.storeCapacity) { // storage is almost full
-                                structure = creep.room.terminal;
-                            }
-                        }
-                    }
-
-                    if (creep.transfer(structure, resourceType) == ERR_NOT_IN_RANGE) {
-                        creep.travelTo(structure, {
-                            maxRooms: 1
-                        });
-                    } else if (structure !== undefined) {
-                        if (creep.transfer(structure, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
-                            creep.travelTo(structure);
-                        }
-                    }
+       else {
+            var structure = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
+                filter: (s) => (s.structureType === STRUCTURE_SPAWN
+                    || s.structureType === STRUCTURE_EXTENSION
+                    || s.structureType === STRUCTURE_NUKER)
+                    && s.energy < s.energyCapacity
+            });
+            if (structure !== undefined) {
+                if (creep.transfer(structure, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+                    creep.travelTo(structure);
                 }
             }
         }
