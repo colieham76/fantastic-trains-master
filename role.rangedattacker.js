1f0e136b3bb70e3cffc1d731622eb3cfb1b3c0e7
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-06-19 11:39:47 +0100

    Update role.rangedattacker.js

--- role.rangedattacker.js
+++ role.rangedattacker.js
@@ -10,70 +10,42 @@ module.exports = {
 			}
 			return;
 		}
-
 		if (!creep.memory.W1S7 && creep.room.name === 'W1S7' || creep.room.name === 'W1S8') {
 			creep.moveTo(Game.flags['W1S7']);
 			if (creep.pos.isNearTo(Game.flags['W1S7'])) {
 				creep.memory.W1S7 = true;
 			}
 			return;
+		} 
+		var attacked;
+		var targets = [];
+	        //attack within range	
+		// attack creeps in range (ranged)
+		if (creep.getActiveBodyparts(RANGED_ATTACK)) {
+			targets = creep.pos.findInRange(FIND_HOSTILE_CREEPS, 40);
+			if (targets.length > 0) {
+				attacked = creep.rangedAttack(targets[0]);
+				if (attacked == ERR_NOT_IN_RANGE)
+					creep.moveTo(targets[0]);
+			}	
 		}
-	    
-/*
-		if (!creep.memory.invader1 && creep.room.name === 'W62N69') {
-			creep.moveTo(Game.flags['invader1']);
-			if (creep.pos.isNearTo(Game.flags['invader1'])) {
-				creep.memory.invader1 = true;
-			}
-			return;
+		else if(creep.pos.isNearTo(targets) && creep.getActiveBodyParts(ATTACK) > 0) {
+			creep.attack(targets);
 		}
-
-		if (!creep.memory.RFlag1 && creep.room.name === 'W62N69' 
-                  || creep.room.name === 'W62N68' 
-                  || creep.room.name === 'W63N69'){
-			creep.moveTo(Game.flags['RFlag1']);
-			if (creep.pos.isNearTo(Game.flags['RFlag1'])) {
-				creep.memory.RFlag1 = true;
-			}
-			return;
+		if(creep.hits < creep.memory.lastHits) {
+			Game.notify('Ranged attacker '+creep+' has been attacked at '+creep.pos+'! at tick number'+Game.time+'!', 180);
 		}
-*/
-	    
-	    
-	    
-	var attacked;
-        var targets = [];
-        //attack within range
-
-	//attack creeps in range (ranged)
-        if (creep.getActiveBodyparts(RANGED_ATTACK)) {
-            targets = creep.pos.findInRange(FIND_HOSTILE_CREEPS, 40);
-            if (targets.length > 0) {
-                attacked = creep.rangedAttack(targets[0]);
-                if (attacked == ERR_NOT_IN_RANGE)
-                    creep.moveTo(targets[0]);
-
-            }
-	    
-	} 
-	    
-	    if(creep.hits < creep.memory.lastHits) {
-            Game.notify('Ranged attacker '+creep+' has been attacked at '+creep.pos+'! at tick number'+Game.time+'!', 180);
-            }
-        creep.memory.lastHits = creep.hits;
-
-       if(creep.hits < 1) {
-            Game.notify('Ranged attacker '+creep+' has been killed at '+creep.pos+'! at tick number'+Game.time+'!', 180);
-            } 
-	    
-	    
+		creep.memory.lastHits = creep.hits;
+		if(creep.hits < 1) {
+			Game.notify('Ranged attacker '+creep+' has been killed at '+creep.pos+'! at tick number'+Game.time+'!', 180);
+		}       
 		let hostileStructure = creep.pos.findClosestByRange(FIND_HOSTILE_STRUCTURES);
 		if (hostileStructure) {
 			if (creep.attack(hostileStructure) === ERR_NOT_IN_RANGE) {
 				creep.travelTo(hostileStructure);
-			}
+			}		
 		}
-	}
+    }
 };
 /*	    
 	    
