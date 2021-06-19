9b738e0c1715e5d8ab742b6912216389ec242ce4
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-06-19 05:31:46 +0100

    Update role.dismantler.js

--- role.dismantler.js
+++ role.dismantler.js
@@ -4,10 +4,12 @@ module.exports = {
             if (Game.flags['Dismantle'] != undefined) {
                           let presious = getTargetByFlag('Dismantle','structure');
                           if (presious != undefined) { // if there is storage
+
                               if (creep.dismantle(presious) == ERR_NOT_IN_RANGE) {
                                   creep.travelTo(presious);
                               }
                           }
-                          
+                   }
+}       
                             
      
