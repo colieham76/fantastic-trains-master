e9d2e65484a12e918a28ade3c816b34846ac3d01
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-06-19 05:40:44 +0100

    Update role.dismantler.js

--- role.dismantler.js
+++ role.dismantler.js
@@ -8,7 +8,11 @@ if (Game.flags.Dismantle) {
                 creep.travelTo(Game.flags.Dismantle);
             }
 
-                             
+  var target = creep.pos.findClosestByRange(FIND_HOSTILE_STRUCTURES);
+                            if (creep.dismantle(target) == ERR_NOT_IN_RANGE) {
+                                creep.travelTo(target)
+                            }          
+                 
                           }
                    
 }       
