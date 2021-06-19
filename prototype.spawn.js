4d5ae0aa6aece8a67a91bc831b7010bbd0486f04
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-06-19 06:12:08 +0100

    Update prototype.spawn.js

--- prototype.spawn.js
+++ prototype.spawn.js
@@ -59,6 +59,8 @@ Game.spawns.Spawn4.memory.minLongDistanceHarvesters = {W3S7: 2};
 Game.spawns.Spawn3.memory.minNumberOfreservers = {W3S7: 1}
 Game.spawns.Spawn5.memory.minNumberOfreservers = {W1S7: 1}
 
+Game.spawns.Spawn5.memory.minNumberOfreservers = {W3S8: 1}
+
 Game.spawns.Spawn5.memory.mintowerdrainers1 = {W1S8: 0}
 
 Game.spawns.Spawn5.memory.mintowerdrainers2 = {W1S8: 0}
