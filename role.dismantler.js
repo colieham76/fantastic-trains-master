3095bf657714eed7d34396abab86276eda428b46
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-06-19 06:22:14 +0100

    Update role.dismantler.js

--- role.dismantler.js
+++ role.dismantler.js
@@ -11,7 +11,18 @@ if (Game.flags.Dismantle) {
   var target = creep.pos.findClosestByRange(FIND_HOSTILE_STRUCTURES);
                             if (creep.dismantle(target) == ERR_NOT_IN_RANGE) {
                                 creep.travelTo(target)
-                            }          
+                            }  
+
+  let invaderCreep = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+                
+                    if (creep.room.name === 'W3S8' && Game.time % 50 === 0) {
+                      if (invaderCreep) {
+                        Game.spawns.Spawn2.memory.rangedattackerRoom = 'W3S8';
+                        Game.spawns.Spawn5.memory.rangedattackerRoom = 'W3S8'
+                      }
+                    }
+
+        
                  
                           }
                    
