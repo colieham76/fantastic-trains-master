77651795772b5dbd374d72f7af3fb2ffcbd9732b
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-06-27 18:22:11 +0100

    Update role.onlyMineralHauler.js

--- role.onlyMineralHauler.js
+++ role.onlyMineralHauler.js
@@ -21,21 +21,25 @@ module.exports = {
                 }
                 else { // in mineral room
                     // avoid incoming keeper
-                    let keeperLair = creep.pos.findInRange(FIND_STRUCTURES, avoidRadius, { filter: c => c.structureType == STRUCTURE_KEEPER_LAIR && c.ticksToSpawn < 3 });
+                    let keeperLair = creep.pos.findInRange(FIND_STRUCTURES, avoidRadius, { 
+                        filter: c => c.structureType == STRUCTURE_KEEPER_LAIR && c.ticksToSpawn < 3 });
                     //if ((Game.rooms[creep.memory.target]==undefined)||(Game.rooms[creep.memory.target].memory.ifPeace == false)) { // room under attack, run away
-                    if ((creep.pos.findInRange(FIND_HOSTILE_CREEPS, avoidRadius).length > 0) || keeperLair && (keeperLair.length > 0)) { // self destroy if not useful damages by NPC
+                    if ((creep.pos.findInRange(FIND_HOSTILE_CREEPS, avoidRadius).length > 0) 
+                        || keeperLair && (keeperLair.length > 0)) { // self destroy if not useful damages by NPC
                         actionRunAway.run(creep);
                     }
                     else { // working
                         // find dropped minerals
-                        let droppeds = creep.room.find(FIND_DROPPED_RESOURCES, { filter: c => (c.resourceType != 'energy') });
+                        let droppeds = creep.room.find(FIND_DROPPED_RESOURCES, { 
+                            filter: c => (c.resourceType != 'energy') });
                         if (droppeds.length > 0) {
                             if (creep.pickup(droppeds[0]) == ERR_NOT_IN_RANGE) {
                                 creep.travelTo(droppeds[0], { maxRooms: 1 });
                             }
                         }
                         else {
-                            let tombstones = creep.room.find(FIND_TOMBSTONES, { filter: c => ((_.sum(c.store) > 0) && (c.store.energy == undefined || ((_.sum(c.store) - c.store.energy) > 0))) });
+                            let tombstones = creep.room.find(FIND_TOMBSTONES, { 
+                                filter: c => ((_.sum(c.store) > 0) && (c.store.energy == undefined || ((_.sum(c.store) - c.store.energy) > 0))) });
                             if (tombstones.length > 0) {
                                 for (let mineralType in tombstones[0].store) {
                                     if (creep.withdraw(tombstones[0], mineralType) == ERR_NOT_IN_RANGE) {
