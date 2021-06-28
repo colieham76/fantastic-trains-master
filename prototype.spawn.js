56a5abd1d65fe48831239ac5f86ba7a39d7bc485
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-06-28 21:02:21 +0100

    Update prototype.spawn.js

--- prototype.spawn.js
+++ prototype.spawn.js
@@ -39,9 +39,9 @@ Game.spawns.Spawn5.memory.minCreeps = {harvester: 1, upgrader: 0, wallRepairer: 
 dismantler: 0, antiTransporter: 0};
 Game.spawns.Spawn2.memory.minLongDistanceHarvesters = {W1S9: 1};
 //if (Game.time % 50 === 0) {
-    Game.spawns.Spawn3.memory.minhealers = {W7S6: 1};
+    Game.spawns.Spawn3.memory.minhealers = {W7S6: 0};
 //}
-Game.spawns.Spawn4.memory.minhealers2 = {W7S6: 0};
+Game.spawns.Spawn4.memory.minhealers2 = {W7S6: 1};
 
 Game.spawns.Spawn4.memory.minhealers3 = {W7S6: 0};
 
@@ -67,12 +67,12 @@ Game.spawns.Spawn5.memory.minNumberOfreservers = {W1S7: 1}
 
 Game.spawns.Spawn2.memory.minNumberOfreservers = {W1S9: 1}
 //if (Game.time % 1000 === 0) {
-Game.spawns.Spawn3.memory.mintowerdrainers1 = {W7S6: 0}
+Game.spawns.Spawn3.memory.mintowerdrainers1 = {W7S6: 1}
 //}
 
 //if (Game.time % 1000 === 0) {
 
-Game.spawns.Spawn4.memory.mintowerdrainers2 = {W7S6: 0}
+Game.spawns.Spawn4.memory.mintowerdrainers2 = {W7S6: 1}
 //}
 Game.spawns.Spawn5.memory.mintowerdrainers3 = {W1S8: 0}
 
