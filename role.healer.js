62b95457334a432a71adf6b56083532316380208
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-06-30 12:44:06 +0100

    Update role.healer.js

--- role.healer.js
+++ role.healer.js
@@ -3,7 +3,7 @@ module.exports = {
     run: function(creep) {
      //   creep.say('💕', true);
 
-      if (creep.ticksToLive < 1500) {
+      if (creep.ticksToLive < 350) {
 Game.spawns.Spawn3.memory.minhealers = {W7S6: 2};
 }  
         
