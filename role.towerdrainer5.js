be4769665f08730e8ccf8f894821fc37ec478b44
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-06-30 15:33:21 +0100

    Update role.towerdrainer5.js

--- role.towerdrainer5.js
+++ role.towerdrainer5.js
@@ -4,11 +4,9 @@ module.exports = {
 
     run: function (creep) {
 
-if (creep.ticksToLive < 350) {
-
-Game.spawns.Spawn3.memory.mintowerdrainers5 = {W6S7: 2}
-
-  }      
+//if (creep.ticksToLive < 350) {
+//Game.spawns.Spawn3.memory.mintowerdrainers5 = {W6S7: 2}
+//}      
 
         
         /*
