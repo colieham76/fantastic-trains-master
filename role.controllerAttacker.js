310a8e06f5cab36c970bb20b1c7c6b14b7bc4a88
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-06-30 19:44:25 +0100

    Update role.controllerAttacker.js

--- role.controllerAttacker.js
+++ role.controllerAttacker.js
@@ -1,6 +1,38 @@
 module.exports = {
     run: function(creep) {
         creep.say('OMG!😂', true);
+        
+        if (!creep.memory.w3s8f3) {
+            creep.travelTo(Game.flags['w3s8f3']);
+            if (creep.pos.isNearTo(Game.flags['w3s8f3'])) {
+                creep.memory.w3s8f3 = true;
+            }
+            return;
+        }
+        if (creep.room.name !== creep.memory.target && !creep.memory.w5s7f1) {
+            creep.travelTo(Game.flags['w5s7f1']);
+            if (creep.pos.isNearTo(Game.flags['w5s7f1'])) {
+                creep.memory.w5s7f1 = true;
+            }
+            return;
+        }
+        
+        if (!creep.memory.w5s7f2) {
+            creep.travelTo(Game.flags['w5s7f2']);
+            if (creep.pos.isNearTo(Game.flags['w5s7f2'])) {
+                creep.memory.w5s7f2 = true;
+            }
+            return;
+        }
+//w7s6 dismantle
+        if (!creep.memory.w7s6f1) {
+            creep.travelTo(Game.flags['w7s6f1']);
+            if (creep.pos.isNearTo(Game.flags['w7s6f1'])) {
+                creep.memory.w7s6f1 = true;
+            }
+            return;
+        } 
+        
         if (creep.room.name == creep.memory.target) { // if in target room
             if (creep.room.controller && !creep.room.controller.my) {
                 if (creep.attackController(creep.room.controller) == ERR_NOT_IN_RANGE) {
