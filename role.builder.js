232c38000c4100c22b733928b37138d4e60ef918
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-07-01 20:22:40 +0100

    Update role.builder.js

--- role.builder.js
+++ role.builder.js
@@ -6,44 +6,66 @@ module.exports = {
     /** @param {Creep} creep *
      */
     run: function (creep) {
-        /*
-        if (!creep.memory.w3s8f1) {
-            creep.travelTo(Game.flags['w3s8f1']);
-            if (creep.pos.isNearTo(Game.flags['w3s8f1'])) {
-                creep.memory.w3s8f1 = true;
+       
+        if (!creep.memory.w10s8f1) {
+            creep.travelTo(Game.flags['w10s8f1']);
+            if (creep.pos.isNearTo(Game.flags['w10s8f1'])) {
+                creep.memory.w10s8f1 = true;
             }
             return;
         }
-        if (creep.room.name !== creep.memory.target && !creep.memory.w3s8f2) {
-            creep.travelTo(Game.flags['w3s8f2']);
-            if (creep.pos.isNearTo(Game.flags['w3s8f2'])) {
-                creep.memory.w3s8f2 = true;
+//W10S5 dismantler route flag
+        if (!creep.memory.w10s5f1) {
+            creep.travelTo(Game.flags['w10s5f1']);
+            if (creep.pos.isNearTo(Game.flags['w10s5f1'])) {
+                creep.memory.w10s5f1 = true;
+            }
+            return;
+        }
+//W9S5 dismantler route flag
+        if (!creep.memory.w9s5f1) {
+            creep.travelTo(Game.flags['w9s5f1']);
+            if (creep.pos.isNearTo(Game.flags['w9s5f1'])) {
+                creep.memory.w9s5f1 = true;
+            }
+            return;
+        }
+//W9S6 dismantler route flag
+        if (!creep.memory.w9s6f1) {
+            creep.travelTo(Game.flags['w9s6f1']);
+            if (creep.pos.isNearTo(Game.flags['w9s6f1'])) {
+                creep.memory.w9s6f1 = true;
             }
             return;
         }
 
-        if (!creep.memory.w3s8f3) {
-            creep.travelTo(Game.flags['w3s8f3']);
-            if (creep.pos.isNearTo(Game.flags['w3s8f3'])) {
-                creep.memory.w3s8f3 = true;
-            }
-            return;
+        const constructionSites = creep.room.find(FIND_CONSTRUCTION_SITES);
+        
+        if (creep.memory.building && creep.store[RESOURCE_ENERGY] === 0) {
+            creep.memory.building = false;
+             creep.say('🔄 harvest');
         }
-        if (creep.room.name !== creep.memory.target && !creep.memory.w3s8f4) {
-            creep.travelTo(Game.flags['w3s8f4']);
-            if (creep.pos.isNearTo(Game.flags['w3s8f4'])) {
-                creep.memory.w3s8f4 = true;
-            }
-            return;
+        if (!creep.memory.building && creep.store.getFreeCapacity() === 0) {
+            creep.memory.building = true;
+            creep.say('🚧 build');
         }
 
-        if (!creep.memory.w3s8f5) {
-            creep.travelTo(Game.flags['w3s8f5']);
-            if (creep.pos.isNearTo(Game.flags['w3s8f5'])) {
-                creep.memory.w3s8f5 = true;
+        if (creep.memory.building) {
+            if (constructionSites.length > 0) {
+                if (creep.build(constructionSites[0]) === ERR_NOT_IN_RANGE) {
+                    creep.travelTo(constructionSites[0]);
+                }
+            } else {
+                roleUpgrader.run(creep);
             }
-            return;
-        }*/
+        } else {
+            if (!creep.memory.building) {
+                (creep.getEnergy(true, true));
+            }
+        }
+    }
+}
+
         const constructionSites = creep.room.find(FIND_CONSTRUCTION_SITES);
         
         if (creep.memory.building && creep.store[RESOURCE_ENERGY] === 0) {
