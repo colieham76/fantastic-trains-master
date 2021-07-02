7ff4f49c9951d80d73ffebccd49f503fa906e3ae
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-07-02 06:31:11 +0100

    Update prototype.spawn.js

--- prototype.spawn.js
+++ prototype.spawn.js
@@ -38,7 +38,7 @@ Game.spawns.Spawn3.memory.mincontrollerattackers = {W7S7: 1}
 Game.spawns.Spawn5.memory.minCreeps = {harvester: 1, upgrader: 0, wallRepairer: 0,
 dismantler: 0, antiTransporter: 0};
 Game.spawns.Spawn6.memory.minCreeps = {harvester: 1, upgrader: 1, wallRepairer: 0,
-dismantler: 0, antiTransporter: 0};
+dismantler: 0, towerlorry: 1 };
 
 Game.spawns.Spawn2.memory.minLongDistanceHarvesters = {W1S9: 1};
 //if (Game.time % 50 === 0) {
