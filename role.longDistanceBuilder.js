1b1a0737510c47f30ab49d1e3ed767f0f748dfd2
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-07-07 11:17:58 +0100

    Update role.longDistanceBuilder.js

--- role.longDistanceBuilder.js
+++ role.longDistanceBuilder.js
@@ -3,31 +3,19 @@ var actionBuild = require('action.build');
 
 module.exports = {
     run: function(creep) {
-      if ((Game.rooms[creep.memory.target]==undefined)
-          ||(Game.rooms[creep.memory.target].memory.ifPeace == false)) { // room under attack, run away
-         creep.say('run away');
-         if (creep.memory.home == undefined) {
-               creep.travelTo(Game.flags[creep.memory.target+'_shelter'].pos);
-         }
-         else {
-               if (creep.room.name != creep.memory.home) { // if not at home base
-                   creep.travelTo(new RoomPosition(25,25, creep.memory.home));
-               }
-               else if (creep.room.name == creep.memory.home) {
-                   roleBuilder.run(creep);
-               }
-         }
-      }
-      else {
-        creep.say('build here die here');
-        if (creep.room.name == creep.memory.target) {           
+        if (creep.room.name != creep.memory.home) { // if not at home base
+            creep.travelTo(Game.flags[creep.memory.target].pos);
+        }
+        else if (creep.room.name == creep.memory.home) {
+            roleBuilder.run(creep);
+        }                 
+        if (creep.room.name == creep.memory.target) { 
+            creep.say('build here die here');
             roleBuilder.run(creep);
         }
         else { // go to target room
             var exit = creep.room.findExitTo(creep.memory.target);
             creep.travelTo(creep.pos.findClosestByRange(exit));
-            //creep.travelTo(Game.flags[creep.memory.target].pos);           
-        }
-      }
+        }        
     }
 };
