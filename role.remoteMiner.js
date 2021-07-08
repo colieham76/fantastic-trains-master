f2f0f87892abc8bd3ee0afbed3a9a126ad9dc627
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-07-08 14:49:10 +0100

    Create role.remoteMiner.js

---
+++ role.remoteMiner.js
@@ -0,0 +1,25 @@
+module.exports = {
+	run: function (creep) {
+		if (creep.room.name == creep.memory.target) {
+			let source = Game.getObjectById(creep.memory.sourceId);
+			let container = source.pos.findInRange(FIND_STRUCTURES, 1, {
+				filter: (s) => s.structureType == STRUCTURE_CONTAINER,
+			})[0];
+
+			if (container != undefined) {
+				if (creep.pos.isEqualTo(container.pos)) {
+					creep.harvest(source);
+				} else {
+					creep.moveTo(container);
+				}
+			} else {
+				if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
+					creep.moveTo(source);
+				}
+			}
+		} else {
+			let exit = creep.room.findExitTo(creep.memory.target);
+			creep.moveTo(creep.pos.findClosestByRange(exit));
+		}
+	},
+};
