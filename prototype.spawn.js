babfcd22eb9e833aeefc742b9e5b2e884cd570a8
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-07-14 15:04:26 +0100

    Update prototype.spawn.js

--- prototype.spawn.js
+++ prototype.spawn.js
@@ -103,8 +103,8 @@ Game.spawns.Spawn1.memory.mintowerdrainers5 = {W5S9: 0};
 //if (Game.time % 1300 === 0) {
 Game.spawns.Spawn2.memory.mincontrollerattackers = {W2S8: 0};
 //}
-Game.spawns.Spawn7.memory.minCreeps = {harvester: 1, towerlorry: 1, upgrader: 0,
-				       rampartrepairer: 0, builder: 1};
+Game.spawns.Spawn7.memory.minCreeps = {harvester: 0, towerlorry: 1, upgrader: 3,
+				       rampartrepairer: 1, lorry: 3, builder: 0};
 /*
 Game.spawns.Spawn5.memory.mintowerdrainers1 = {W1S8: 1}
 
