bf8fd4fca592a61dfd4622695dda64578fd1ee44
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-07-18 11:44:34 +0100

    Update role.controllerAttacker.js

--- role.controllerAttacker.js
+++ role.controllerAttacker.js
@@ -6,7 +6,10 @@ module.exports = {
            Game.spawns.Spawn6.memory.mincontrollerattackers = {W9S5: 2} 
         }    */  
         
-            
+       if (creep.ticksToLive < 1500) {
+                  creep.suicide();
+          }
+     
         if (creep.room.name == creep.memory.target) { // if in target room
             if (creep.room.controller && !creep.room.controller.my) {
                 if (creep.attackController(creep.room.controller) == ERR_NOT_IN_RANGE) {
