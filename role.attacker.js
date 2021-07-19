18eab4b803a9b097a0ba7c3660159f3fdea707ba
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-07-19 12:12:43 +0100

    Update role.attacker.js

--- role.attacker.js
+++ role.attacker.js
@@ -1,14 +1,13 @@
 module.exports = {
     run: function(creep) {
         creep.say('OMG!😨', true);
-        
-if (!creep.memory.w9s5f1) {
+        if (!creep.memory.w9s5f1) {
             creep.travelTo(Game.flags['w9s5f1']);
             if (creep.pos.isNearTo(Game.flags['w9s5f1'])) {
                 creep.memory.w9s5f1 = true;
             }
             return;
-        }   
+        }
         /*if (grouped && healingEnough) {
             // move forward
         }
@@ -16,34 +15,32 @@ if (!creep.memory.w9s5f1) {
             // move backward
         }*/
 
-        if (creep.hits > 0.9*creep.hitsMax) { // if full health
+        if (creep.hits > 0.9*creep.hitsMax) {
             let myNurseName = findMyHero(creep.memory.uniqueString, 'healer');
             let myNurse = Game.creeps[myNurseName];
             //if (creep.pos.findInRange(FIND_MY_CREEPS, 1, {
             //filter: s => ((s.memory.role=='healer')
             //&&(s.memory.uniqueString==creep.memory.uniqueString))}).length>0) {
-            if ((creep.pos.getRangeTo(myNurse)<2)||(creep.pos.x==0)
+            if ((creep.pos.getRangeTo(myNurse)<40)||(creep.pos.x==0)
                 ||(creep.pos.y==0)
                 ||(creep.pos.x==49)
                 ||(creep.pos.y==49)) {
-                if (creep.room.name != creep.memory.target) { // if not in target room
-                   creep.travelTo(Game.flags[creep.memory.target]);
+                if (creep.room.name != creep.memory.target) {
+                    creep.travelTo(Game.flags[creep.memory.target]);
                 }
-                else { // in target room
-                    if (Game.flags[creep.memory.target+'attack'] != undefined) { // && creep.getActiveBodyparts(ATTACK)>0) {
-                        creep.travelTo(Game.flags[creep.memory.target+'attack']); // gether at flag's position
-                        
+                else {//if in target room
+                    if (Game.flags[creep.memory.target+'attack'] != undefined) {// && creep.getActiveBodyparts(ATTACK)>0) {
+                        creep.travelTo(Game.flags[creep.memory.target+'attack']);    // gether at flag's position
                         let target = Game.flags[creep.memory.target+'attack'].pos.findInRange(FIND_STRUCTURES, 0)[0];
 
                         //var target = Game.getObjectById(Game.flags['attack'].room.lookAt(2,17)[0]['structure'].id);
                         //console.log(target)
 
                         if (creep.attack(target) == ERR_NOT_IN_RANGE) {
-                            creep.rangedAttackattack(target)
+                            creep.rangedAttack(target)
                             creep.travelTo(target)
                         }
-
-                        if (creep.pos.isEqualTo(Game.flags[creep.memory.target+'attack'])) {
+                        if (creep.pos.isEqualTo(Game.flags[creep.memory.target+'attack'])) {                           
                             Game.flags[creep.memory.target+'attack'].remove();
                         }
                     }
@@ -52,7 +49,7 @@ if (!creep.memory.w9s5f1) {
                             filter: c => ((c.pos.getRangeTo(creep) < 48))});
                         if (target) {
                             if (creep.attack(target) == ERR_NOT_IN_RANGE) {
-                                creep.rangedAttackattack(target);
+                                creep.rangedAttack(target);
                                 creep.attack(target);
                                 creep.travelTo(target);
                             }
@@ -71,11 +68,12 @@ if (!creep.memory.w9s5f1) {
                                     creep.rangedAttackattack(target);
                                 }
                                 else {
-                                    core = creep.room.find(FIND_STRUCTURES, {filter:c => c.structureType==STRUCTURE_CONTAINER})[0];
+                                    core = creep.room.find(FIND_STRUCTURES, {
+                                        filter:c => c.structureType==STRUCTURE_CONTAINER})[0];
                                     if (core) {
                                         creep.travelTo(core);
                                         creep.attack(core);
-                                        creep.rangedAttackattack(target);
+                                        creep.rangedAttack(target);
                                     }
                                 }
                             }
@@ -87,7 +85,7 @@ if (!creep.memory.w9s5f1) {
                 creep.travelTo(Game.flags[creep.memory.target+'esc']);
             }
         }
-        else { // wounded
+        else {
             creep.travelTo(Game.flags[creep.memory.target+'esc']);
         }
     }
