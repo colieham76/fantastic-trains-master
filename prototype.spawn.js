37b990e71c7b1f632dea388aecc9b986759fa651
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-07-21 07:46:22 +0100

    Update prototype.spawn.js

--- prototype.spawn.js
+++ prototype.spawn.js
@@ -30,7 +30,7 @@ Game.spawns.Spawn7.memory.minrangedattackers = {'W8S6': 0};
 
 Game.spawns.Spawn1.memory.minCreeps = {repairer: 0, builder: 0,
  lorry: 2, towerlorry: 1, dismantler: 0,
-    upgrader: 0, extractor: 0};
+    upgrader: 0, extractor: 1};
 Game.spawns.Spawn1.memory.minLongDistanceHarvesters = {W7S9: 1};
 Game.spawns.Spawn1.memory.minNumberOfreservers = {W7S9: 1};
 Game.spawns.W7S8.memory.minCreeps = {harvester: 0, rampartrepairer: 0};
@@ -43,8 +43,8 @@ Game.spawns.Spawn2.memory.minCreeps = {repairer: 0, builder: 0, lorry: 2, towerl
 Game.spawns.Spawn3.memory.mincontrollerattackers = {W7S7: 0} 
 Game.spawns.Spawn5.memory.minCreeps = {harvester: 0, upgrader: 0, wallRepairer: 0,
 dismantler: 0, antiTransporter: 0};
-Game.spawns.Spawn6.memory.minCreeps = {harvester: 0, upgrader: 1, repairer: 0, 
-wallRepairer: 0, extractor: 1, builder: 0, rampartrepairer: 0, lorry: 2, towerlorry: 1 };
+Game.spawns.Spawn6.memory.minCreeps = {harvester: 0, upgrader: 0, repairer: 0, 
+wallRepairer: 0, extractor: 0, builder: 1, rampartrepairer: 0, lorry: 2, towerlorry: 1 };
 Game.spawns.Spawn6.memory.minLongDistanceLorrys = {W9S7: 0};
 Game.spawns.Spawn6.memory.minLongDistanceBuilders = {W9S7: 0};
 Game.spawns.Spawn6.memory.minattackers = {W9S5: 0};
@@ -107,8 +107,8 @@ Game.spawns.Spawn1.memory.mintowerdrainers5 = {W9S5: 0};
 //if (Game.time % 1300 === 0) {
 Game.spawns.Spawn6.memory.mincontrollerattackers = {W9S5: 0};
 //}
-Game.spawns.Spawn7.memory.minCreeps = {harvester: 1, towerlorry: 2, upgrader: 2, wallRepairer: 2,
-				       rampartrepairer: 0, lorry: 2, builder: 0, dismantler: 0};
+Game.spawns.Spawn7.memory.minCreeps = {harvester: 1, towerlorry: 2, upgrader: 0, wallRepairer: 2,
+				       rampartrepairer: 0, lorry: 2, builder: 1, dismantler: 0};
 Game.spawns.Spawn7.memory.minLongDistanceHarvesters = {W7S7: 1};
 
 /*
