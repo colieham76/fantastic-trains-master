cd00720e072d38749f6ca65e53b21e5c54f855ad
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-07-21 07:40:33 +0100

    Update role.upgrader.js

--- role.upgrader.js
+++ role.upgrader.js
@@ -2,35 +2,59 @@ module.exports = {
     // a function to run the logic for this role
     /** @param {Creep} creep */
     run: function (creep) {
-
-
-        var terminalVar = creep.room.terminal;
-        var storageVar = creep.room.storage
-
         if (creep.memory.working && creep.store.energy == 0) {
             creep.memory.working = false;
         } else if (!creep.memory.working && creep.store.energy == creep.store.getCapacity()) {
             creep.memory.working = true;
+        }        
+        //W10S8 upgrader route flag        
+        if (!creep.memory.w10s8f1) {
+            creep.travelTo(Game.flags['w10s8f1']);
+            if (creep.pos.isNearTo(Game.flags['w10s8f1'])) {
+                creep.memory.w10s8f1 = true;
+            }
+            return;
         }
-
-        // if creep is supposed to transfer energy to the controller
+        //W10S5 upgrader route flag
+        if (!creep.memory.w10s5f1) {
+            creep.travelTo(Game.flags['w10s5f1']);
+            if (creep.pos.isNearTo(Game.flags['w10s5f1'])) {
+                creep.memory.w10s5f1 = true;
+            }
+            return;
+        }
+        //W9S5 upgrader route flag
+        if (!creep.memory.w9s5f1) {
+            creep.travelTo(Game.flags['w9s5f1']);
+            if (creep.pos.isNearTo(Game.flags['w9s5f1'])) {
+                creep.memory.w9s5f1 = true;
+            }
+            return;
+        }
+        if (!creep.memory.w8s6f1) {
+            creep.travelTo(Game.flags['w8s6f1']);
+            if (creep.pos.isNearTo(Game.flags['w8s6f1'])) {
+                creep.memory. w8s6f1 = true;
+            }
+            return;
+        }
+        if (!creep.memory.w7s6f1) {
+            creep.travelTo(Game.flags['w7s6f1']);
+            if (creep.pos.isNearTo(Game.flags['w7s6f1'])) {
+                creep.memory. w7s6f1 = true;
+            }
+            return;
+        }
         if (creep.memory.working == true) {
-            // instead of upgraderController we could also use:
             if (creep.transfer(creep.room.controller, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-
-                // try to upgrade the controller
-                //if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-                // if not in range, move towards the controller
                 creep.travelTo(creep.room.controller);
             } else if (creep.room.controller && creep.room.controller.my) {
                 if (creep.signController(creep.room.controller, "There are none more hopelessly " +
-                    "enslaved than those who falsely believe they are free!") == ERR_NOT_IN_RANGE)
-                    // if not in range, move towards the controller
+                                         "enslaved than those who falsely believe they are free!") == ERR_NOT_IN_RANGE)
                     creep.travelTo(creep.room.controller);
-            }
+            }   
         } else {
-            //withdraw energy from terminal first
-                creep.getEnergy(true, true);
+            creep.getEnergy(true, true);
         }
     }
 }
