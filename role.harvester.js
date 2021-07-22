7ac267158a9248cb21feb7bce56ec5f0cc76dd76
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-07-22 20:33:55 +0100

    Update role.harvester.js

--- role.harvester.js
+++ role.harvester.js
@@ -16,7 +16,7 @@ module.exports = {
             
             // specific harvester task - take dropped energy and put into storage only
 //remove comment slash from here
-
+/*
             if(creep.room.storage
                 && creep.room.storage.store.getUsedCapacity() < creep.room.storage.store.getCapacity()) {
                 var storage = creep.room.storage;
@@ -25,13 +25,14 @@ module.exports = {
                         creep.moveTo(storage, {visualizePathStyle: {stroke: '#fcfafa'}});
                     }
                 }
+*/
             // to here     
                 ////////////////////////////
          // normal harvester code - find closest spawn, remove comment slashes below and insert comment slash to exclude bracket line 50
          // extension or tower which is not full
 ////////////////
 // from here
-            /*
+            
          var structure = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
                               
                     filter: (s) => (s.structureType === STRUCTURE_EXTENSION
@@ -46,8 +47,8 @@ module.exports = {
                     creep.travelTo(structure);
                 }
             }  ////To here - remove comment slash for bracket line 50 to switch on dropped resource utiliser
-*/
-            }
+
+       //     }
         }
         // if creep is supposed to harvest energy from source
         else {
