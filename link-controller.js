c45b669656b3b6f67e721b6025bd30be99514599
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-07-23 19:09:52 +0100

    Update link-controller.js

--- link-controller.js
+++ link-controller.js
@@ -3,17 +3,17 @@ var linkController = {
 
     run: function(link) {
 
-        const upgradeLink = Game.getObjectById('60f7cd829709c9966154d996'); //remote mining link in W71N69
+      //  const upgradeLink = Game.getObjectById('60f7cd829709c9966154d996'); //remote mining link in W71N69
         const upgradeLink2 = Game.getObjectById('60f7d1093bd3cc14ace13cfa'); //remote mining link in W62N61
         
            
-        const upgradeLinkEnergyNeeded = upgradeLink.energyCapacity - upgradeLink.energy;
+     //   const upgradeLinkEnergyNeeded = upgradeLink.energyCapacity - upgradeLink.energy;
         const upgradeLinkEnergyNeeded2 = upgradeLink2.energyCapacity - upgradeLink2.energy;
         
     if (Game.time % 5 == 0) {    
-        if (link.energy >= upgradeLinkEnergyNeeded) {
-            link.transferEnergy(upgradeLink, upgradeLinkEnergyNeeded)
-        }
+     //   if (link.energy >= upgradeLinkEnergyNeeded) {
+      //      link.transferEnergy(upgradeLink, upgradeLinkEnergyNeeded)
+       // }
            
         if (link.energy >= upgradeLinkEnergyNeeded2) {
             link.transferEnergy(upgradeLink2, upgradeLinkEnergyNeeded2)
