6685d085cdb44477b5dcf97e2d2de53b61ccc837
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-07-23 23:55:32 +0100

    Update role.lorry.js

--- role.lorry.js
+++ role.lorry.js
@@ -1,4 +1,4 @@
-       module.exports = {
+module.exports = {
     // a function to run the logic for this role
     /** @param {Creep} creep */
     run: function (creep) {
@@ -14,40 +14,26 @@
 
         // if creep is supposed to transfer energy to a structure
         if (creep.memory.working !== true) {
-
-           
-                var [resourceID, ifDropped] = evaluateEnergyResources(creep, false, false,
-                    true, true); // find energy function in myFunctions
-                if (resourceID !== undefined) {
-                    energy = Game.getObjectById(resourceID);
-                    if (ifDropped) { // if energy is dropped
-                        if (creep.pickup(energy) === ERR_NOT_IN_RANGE) {
-                            creep.moveTo(energy);
-                        }
-                    } else { // energy is from container, storage or link
-                        if (creep.withdraw(energy, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
-                            creep.moveTo(energy);
-                        }
+            var [resourceID, ifDropped] = evaluateEnergyResources(creep, false, false,
+                true, true); // find energy function in myFunctions
+            if (resourceID !== undefined) {
+                energy = Game.getObjectById(resourceID);
+                if (ifDropped) { // if energy is dropped
+                    if (creep.pickup(energy) === ERR_NOT_IN_RANGE) {
+                        creep.moveTo(energy);
+                    }
+                } else { // energy is from container, storage or link
+                    if (creep.withdraw(energy, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+                        creep.moveTo(energy);
                     }
                 }
             }
-        
-}
-     //   else {
 
-if (creep.room.name === 'W7S6') {
-                creep.moveTo(31, 31, {visualizePathStyle: {stroke: '#ffaa00'}});
-                if (creep.pos == 31, 31) {
-                    const upgradeContainer = Game.getObjectById('60f7d1093bd3cc14ace13cfa');
-                    if (upgradeContainer.energy > 0) {
-                        if (creep.withdraw(upgradeContainer, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
-                            creep.travelTo(upgradeContainer);
-                        }
-                    }
-                }
-            } 
-//}
 
+
+        }
+
+        else {
             var structure = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
                 filter: (s) => (s.structureType === STRUCTURE_SPAWN
                     || s.structureType === STRUCTURE_EXTENSION
@@ -64,10 +50,10 @@ if (creep.room.name === 'W7S6') {
         var terminal = creep.pos.findClosestByRange(FIND_MY_STRUCTURES, {
             filter: s => s.structureType == STRUCTURE_TERMINAL && s.store[RESOURCE_ENERGY] <= 150000
         });
+
         if (creep.transfer(terminal, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
             creep.moveTo(terminal);
         } 
         */
     }
-};     
-   
+};    
