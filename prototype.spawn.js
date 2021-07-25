db0f4027095a4f4556128eb98db31802cf788e49
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-07-25 11:16:29 +0100

    Update prototype.spawn.js

--- prototype.spawn.js
+++ prototype.spawn.js
@@ -176,8 +176,8 @@ function () {
         //  create a backup creep
         if (numberOfCreeps['harvester'] == 0 && numberOfCreeps['lorry'] == 0) {
             // if there are still miners or enough energy in Storage left
-            if (numberOfCreeps['miner'] > 0// ){
-|| (room.storage != undefined && room.storage.store[RESOURCE_ENERGY] >= 150 + 550)) {
+            if (numberOfCreeps['miner'] > 0 ){
+//|| (room.storage != undefined && room.storage.store[RESOURCE_ENERGY] >= 150 + 550)) {
                 // create a lorry
                 name = this.createLorry(150);
             }
