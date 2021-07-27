760b8ab93c81a28e0582cff339cb1114e967efd1
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-07-27 22:11:14 +0100

    Update role.lorry.js

--- role.lorry.js
+++ role.lorry.js
@@ -18,7 +18,7 @@ module.exports = {
 
         // if creep is supposed to transfer energy to a structure
         if (creep.memory.working !== true) {
-            var [resourceID, ifDropped] = evaluateEnergyResources(creep, true, true,
+            var [resourceID, ifDropped] = evaluateEnergyResources(creep, true, false,
                 true, true); // find energy function in myFunctions
             if (resourceID !== undefined) {
                 energy = Game.getObjectById(resourceID);
@@ -50,7 +50,7 @@ module.exports = {
                 }
             }
         }
-
+/*
         var terminal = creep.pos.findClosestByRange(FIND_MY_STRUCTURES, {
             filter: s => s.structureType == STRUCTURE_TERMINAL && s.store[RESOURCE_ENERGY] <= 150000
         });
@@ -58,6 +58,6 @@ module.exports = {
         if (creep.transfer(terminal, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
             creep.moveTo(terminal);
         } 
-        
+       */ 
     }
 };      
