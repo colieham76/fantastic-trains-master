cc8fbcb0f64ac5236e7e734fe3cf703f2df32b0c
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-07-31 12:27:08 +0100

    Update main.js

--- main.js
+++ main.js
@@ -110,8 +110,8 @@ if(spawn.room.storage && spawn.room.storage.hits < spawn.room.storage.hitsMax) {
 ////////////////////////////////////////////////////////////////////////////////////////
 //MYang2017
 
-	require('./prototype_creep_fight');//tooAngel kiting attacker test
-	require('./prototype_roomPosition'); //tooAngel kiting attacker test
+//	require('./prototype_creep_fight');//tooAngel kiting attacker test
+//	require('./prototype_roomPosition'); //tooAngel kiting attacker test
 
 	require('prototype.spawn');
         require('prototype_creep_heal');
