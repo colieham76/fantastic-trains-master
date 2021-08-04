c6396f56d00c8c745b4c985a7b3fb3a9da9944de
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-08-04 04:25:45 +0100

    Update prototype.spawn.js

--- prototype.spawn.js
+++ prototype.spawn.js
@@ -67,7 +67,7 @@ if(resourcecontainer[0].store[RESOURCE_ENERGY] > 1200){
 
 Game.spawns.Spawn2.memory.minLongDistanceHarvesters = {W1S9: 0};
 //if (Game.time % 50 === 0) {
-Game.spawns.Spawn6.memory.minhealers = {W8S5: 1};
+Game.spawns.Spawn6.memory.minhealers = {W8S5: 0};
 //}
 Game.spawns.Spawn6.memory.minhealers2 = {W8S5: 0};
 
@@ -98,7 +98,7 @@ Game.spawns.Spawn6.memory.mintowerdrainers1 = {W8S5: 1};
 
 //if (Game.time % 1000 === 0) {
 
-Game.spawns.Spawn6.memory.mintowerdrainers2 = {W8S5: 1};
+Game.spawns.Spawn6.memory.mintowerdrainers2 = {W8S5: 0};
 //}
 Game.spawns.Spawn6.memory.mintowerdrainers3 = {W8S5: 0};
 
