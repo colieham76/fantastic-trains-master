1fc477f0e068a11cbe833555220e1669bbb2452b
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-08-04 03:58:40 +0100

    Update prototype.spawn.js

--- prototype.spawn.js
+++ prototype.spawn.js
@@ -93,7 +93,7 @@ Game.spawns.Spawn6.memory.minNumberOfreservers = {W9S5: 1};
 Game.spawns.Spawn7.memory.minNumberOfreservers = {W8S5: 0};
 Game.spawns.Spawn2.memory.minNumberOfreservers = {W1S9: 0};
 //if (Game.time % 1000 === 0) {
-Game.spawns.Spawn6.memory.mintowerdrainers1 = {W8S5: 0};
+Game.spawns.Spawn6.memory.mintowerdrainers1 = {W8S5: 1};
 //}
 
 //if (Game.time % 1000 === 0) {
