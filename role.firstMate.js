4a543cdf772c7257efa064d937d8107be1350117
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-08-05 07:23:05 +0100

    Update role.firstMate.js

--- role.firstMate.js
+++ role.firstMate.js
@@ -1,5 +1,30 @@
 module.exports = {
-    run: function(creep) {        
+    run: function(creep) {   
+        
+         if (!creep.memory.w10s8f1) {
+            creep.travelTo(Game.flags['w10s8f1']);
+            if (creep.pos.isNearTo(Game.flags['w10s8f1'])) {
+                creep.memory.w10s8f1 = true;
+            }
+            return;
+        }
+      if (!creep.memory.w10s2f1) {
+            creep.travelTo(Game.flags['w10s2f1']);
+            if (creep.pos.isNearTo(Game.flags['w10s2f1'])) {
+                creep.memory.w10s2f1 = true;
+            }
+            return;
+        }
+      if (!creep.memory.w9s3f1) {
+            creep.travelTo(Game.flags['w9s3f1']);
+            if (creep.pos.isNearTo(Game.flags['w9s3f1'])) {
+                creep.memory.w9s3f1 = true;
+            }
+            return;
+        }
+
+        
+        
         
          if (creep.room.name != creep.memory.groupName) {
 
