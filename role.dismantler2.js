43f7e5d61f7b5356869fdd66ff1e79a68523912c
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-08-06 09:13:42 +0100

    Update role.dismantler2.js

--- role.dismantler2.js
+++ role.dismantler2.js
@@ -30,38 +30,15 @@ creep.suicide();
             }
             return;
         }
-        /*
 
-if (!creep.memory.w6s6f1) {
-            creep.travelTo(Game.flags['w6s6f1']);
-            if (creep.pos.isNearTo(Game.flags['w6s6f1'])) {
-                creep.memory.w6s6f1 = true;
+        if (!creep.memory.w8s4f1) {
+            creep.travelTo(Game.flags['w8s4f1']);
+            if (creep.pos.isNearTo(Game.flags['w8s4f1'])) {
+                creep.memory.w8s4f1 = true;
             }
             return;
         }
-           if (!creep.memory.w6s5f1) {
-            creep.travelTo(Game.flags['w6s5f1']);
-            if (creep.pos.isNearTo(Game.flags['w6s5f1'])) {
-                creep.memory.w6s5f1 = true;
-            }
-            return;
-        }
-
-            if (!creep.memory.w6s4f1) {
-            creep.travelTo(Game.flags['w6s4f1']);
-            if (creep.pos.isNearTo(Game.flags['w6s4f1'])) {
-                creep.memory.w6s4f1 = true;
-            }
-            return;
-        }
-      if (!creep.memory.w6s3f1) {
-            creep.travelTo(Game.flags['w6s3f1']);
-            if (creep.pos.isNearTo(Game.flags['w6s3f1'])) {
-                creep.memory.w6s3f1 = true;
-            }
-            return;
-        }
-*/
+       
 
 if (Game.flags['Dismantle2'] != undefined) {
             let presious = getTargetByFlag('Dismantle2','structure');
@@ -82,7 +59,7 @@ if (Game.flags['Dismantle2'] != undefined) {
         }
         else {
             let core = creep.pos.findClosestByRange(FIND_HOSTILE_STRUCTURES, {
-                filter:c => c.structureType==STRUCTURE_EXTENSION});
+                filter:c => c.structureType==STRUCTURE_RAMPART});
             if (core==undefined) {
                 core = creep.pos.findClosestByRange(FIND_HOSTILE_STRUCTURES, {
                     filter:c => c.structureType==STRUCTURE_TOWER});
