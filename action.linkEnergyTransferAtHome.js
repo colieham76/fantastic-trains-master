51fbfe19318c82a1f56439ae1cf4a8f05d27018b
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-08-09 16:31:46 +0100

    Update action.linkEnergyTransferAtHome.js

--- action.linkEnergyTransferAtHome.js
+++ action.linkEnergyTransferAtHome.js
@@ -11,18 +11,24 @@ module.exports = {
                     countMax = 2;
                 }
                 else {
-                    countMax = 3;
+                    countMax = 3;                
                 }
                 if (creep.memory.energyTransferCount<countMax) { // transfered a lot and probably not in link range, do not need to scan for link anymore
                     // creep find a link that is close by and transfer energy
-                    let nearbyLinks = creep.pos.findInRange(FIND_MY_STRUCTURES, 1, {filter:s=> ((s.structureType==STRUCTURE_LINK)&&(s.energy<s.energyCapacity))});
+                    let nearbyLinks = creep.pos.findInRange(FIND_MY_STRUCTURES, 1, {
+                        filter:s=> ((s.structureType==STRUCTURE_LINK)
+                            &&(s.energy<s.energyCapacity))
+                    });
                     if (nearbyLinks.length>0&&nearbyLinks[0].energy<800) { // if link is found
                         let nearbyLink = nearbyLinks[0];
                         if (creep.transfer(nearbyLink, RESOURCE_ENERGY)==0) {
                             creep.memory.energyTransferCount += 1;
                         }
                         else {
-                            let nearbyContainers = creep.pos.findInRange(FIND_STRUCTURES, 1, {filter:s=> ((s.structureType==STRUCTURE_CONTAINER)&&(s.energy<s.energyCapacity))});
+                            let nearbyContainers = creep.pos.findInRange(FIND_STRUCTURES, 1, {
+                                filter:s=> ((s.structureType==STRUCTURE_CONTAINER)
+                                    &&(s.energy<s.energyCapacity))
+                            });
                             if (nearbyContainers.length>0) { // if link is found
                                 let nearbyContainer = nearbyContainers[0];
                                 if (creep.transfer(nearbyContainer, RESOURCE_ENERGY)==0) {
@@ -32,7 +38,11 @@ module.exports = {
                         }
                     }
                     else { // no unfilled nearby links or containers
-                        let structure = creep.pos.findClosestByRange(FIND_MY_STRUCTURES, { filter: (s) => ( (s.structureType == STRUCTURE_SPAWN || s.structureType == STRUCTURE_EXTENSION) && s.energy < s.energyCapacity) })
+                        let structure = creep.pos.findClosestByRange(FIND_MY_STRUCTURES, {
+                            filter: (s) => ( (s.structureType == STRUCTURE_SPAWN
+                                || s.structureType == STRUCTURE_EXTENSION)
+                                && s.energy < s.energyCapacity)
+                        });
                         if (structure == undefined) { // cannot find spawn or extensions or tower or they are full, find the storage
                             structure = creep.room.storage;
                         }
@@ -42,7 +52,11 @@ module.exports = {
                     }
                 }
                 else {
-                    let structure = creep.pos.findClosestByRange(FIND_MY_STRUCTURES, { filter: (s) => ( (s.structureType == STRUCTURE_SPAWN || s.structureType == STRUCTURE_EXTENSION) && s.energy < s.energyCapacity) })
+                    let structure = creep.pos.findClosestByRange(FIND_MY_STRUCTURES, { 
+                        filter: (s) => ( (s.structureType == STRUCTURE_SPAWN 
+                            || s.structureType == STRUCTURE_EXTENSION) 
+                            && s.energy < s.energyCapacity) 
+                    });
                     if (structure == undefined) { // cannot find spawn or extensions or tower or they are full, find the storage
                         structure = creep.room.storage;
                     }
@@ -52,7 +66,11 @@ module.exports = {
                 }
             }
             else { // creep is near storage
-                let structure = creep.pos.findClosestByRange(FIND_MY_STRUCTURES, { filter: (s) => ( (s.structureType == STRUCTURE_SPAWN || s.structureType == STRUCTURE_EXTENSION) && s.energy < s.energyCapacity) })
+                let structure = creep.pos.findClosestByRange(FIND_MY_STRUCTURES, { 
+                    filter: (s) => ( (s.structureType == STRUCTURE_SPAWN 
+                        || s.structureType == STRUCTURE_EXTENSION)
+                        && s.energy < s.energyCapacity)
+                });
                 if (structure == undefined) { // cannot find spawn or extensions or tower or they are full, find the storage
                     structure = creep.room.storage;
                 }
@@ -62,7 +80,11 @@ module.exports = {
             }
         }
         else { // if storage is not defined
-            let structure = creep.pos.findClosestByRange(FIND_MY_STRUCTURES, { filter: (s) => ( (s.structureType == STRUCTURE_SPAWN || s.structureType == STRUCTURE_EXTENSION) && s.energy < s.energyCapacity) })
+            let structure = creep.pos.findClosestByRange(FIND_MY_STRUCTURES, { 
+                filter: (s) => ( (s.structureType == STRUCTURE_SPAWN 
+                    || s.structureType == STRUCTURE_EXTENSION) 
+                    && s.energy < s.energyCapacity)
+            });
             if (structure) {
                 if (creep.transfer(structure, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.travelTo(structure);
