b28dd09b9a7b2b8ea72672f308b3bac276dd6438
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-08-09 12:08:57 +0100

    Update role.longDistanceLorry.js

--- role.longDistanceLorry.js
+++ role.longDistanceLorry.js
@@ -4,7 +4,6 @@ var actionRunAway = require('action.flee');
 
 module.exports = {
     run: function(creep) {
-        //console.log(creep.pos,creep.name)
         // repair when walking
         let thingUnderFeet = creep.room.lookForAt(LOOK_STRUCTURES, creep)[0];
         if (thingUnderFeet && thingUnderFeet.structureType == STRUCTURE_ROAD) {
@@ -16,8 +15,7 @@ module.exports = {
                 creep.build(thingUnderFeet);
             }
         }
-
-        if ((creep.pos.findInRange(FIND_HOSTILE_CREEPS, 5).length > 0)) { // self destroy if not useful damages by NPC
+        if ((creep.pos.findInRange(FIND_HOSTILE_CREEPS, 5).length > 0)) {
             actionRunAway.run(creep);
         }
         else {
@@ -32,13 +30,12 @@ module.exports = {
                 creep.memory.working = true;
                 creep.memory.energyTransferCount = 0;
             }
-
             if (creep.memory.working == true) {
                 if (creep.room.name != creep.memory.home) { // if not at home base
                     //creep.travelTo(Game.flags['link' + creep.memory.home]);
                     creep.travelTo(new RoomPosition(25, 25, creep.memory.home));
                 }
-                else { // creep at home
+                else {// creep at home
                     if (creep.memory.working == true && creep.carry.energy == 0) { // < creep.carryCapacity*0.2) {
                         creep.drop(RESOURCE_ENERGY);
                         creep.memory.working = false;
@@ -46,30 +43,32 @@ module.exports = {
                     linkEnergyTransferAtHome.run(creep)
                 }
             }
-            else { // working is false, take energy
-                if (creep.hits< 0.8 * creep.hitsMax) {
+            else {
+                if (creep.hits< 0.8 * creep.hitsMax) {// working is false, take energy
                     creep.travelTo(new RoomPosition(25, 25, creep.memory.home));
                 }
                 else {
-                    if (creep.room.name == creep.memory.target) { // if not in target room go to target room, if in:
-                        let energy = creep.pos.findClosestByRange(FIND_DROPPED_RESOURCES, {filter: {resourceType: RESOURCE_ENERGY}});
+                    if (creep.room.name == creep.memory.target) {// if not in target room go to target room, if in:
+                        let energy = creep.pos.findClosestByRange(FIND_DROPPED_RESOURCES, {
+                            filter: {
+                                resourceType: RESOURCE_ENERGY
+                            }
+                        });
                         if (creep.pickup(energy) == ERR_NOT_IN_RANGE) {
-                            creep.travelTo(energy,{maxRooms:1});
-                        }
-                        else {
+                            creep.travelTo(energy, {maxRooms: 1});
+                        } else {
                             let container = creep.pos.findClosestByRange(FIND_STRUCTURES, {
-                                filter: s => s.structureType == STRUCTURE_CONTAINER && (s.store[RESOURCE_ENERGY] > 500)});
-                            if (container != undefined) { // if all containers are 0, take energy from storage
+                                filter: s => s.structureType == STRUCTURE_CONTAINER
+                                    && (s.store[RESOURCE_ENERGY] > 500)
+                            });
+                            if (container != undefined) {
                                 if (creep.withdraw(container, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                                    creep.travelTo(container,{maxRooms:1});
+                                    creep.travelTo(container, {maxRooms: 1});
                                 }
                             }
                         }
                     }
                     else {
-                        //creep.travelTo(Game.flags[creep.memory.target]);
-                        //var exit = creep.room.findExitTo(creep.memory.target);
-                        //creep.travelTo(creep.pos.findClosestByRange(exit));
                         creep.travelTo(new RoomPosition(25, 25, creep.memory.target));
                     }
                 }
