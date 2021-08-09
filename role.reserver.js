95cbef052c4dca49023c0d6ecf80e7db1334a055
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-08-09 18:51:43 +0100

    Update role.reserver.js

--- role.reserver.js
+++ role.reserver.js
@@ -6,6 +6,14 @@ module.exports = {
 //                  creep.suicide();
 //               }
 
+if (!creep.memory.W9S7 && creep.room.name === 'W9S7'|| creep.room.name === 'W9S6') {
+			creep.travelTo(Game.flags['ReserverW9S7']);
+			if (creep.pos.isNearTo(Game.flags['ReserverW9S7'])) {
+				creep.memory.W9S7 = true;
+			}
+			return
+		}
+
 		if (!creep.memory.W9S5 && creep.room.name === 'W9S5'|| creep.room.name === 'W9S6') {
 			creep.travelTo(Game.flags['ReserverW9S5']);
 			if (creep.pos.isNearTo(Game.flags['ReserverW9S5'])) {
