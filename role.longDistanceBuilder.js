fcd549141a626331f8ee1f0367aea9e0fe080459
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-08-10 23:03:01 +0100

    Update role.longDistanceBuilder.js

--- role.longDistanceBuilder.js
+++ role.longDistanceBuilder.js
@@ -1,16 +1,15 @@
 var roleBuilder = require('role.builder');
 var actionBuild = require('action.build');
-
+var actionRepair = require('action.repair');
 module.exports = {
     run: function(creep) {
-
-
 //if (creep.ticksToLive < 1500) {
  //                  creep.suicide();
    //         }
-
-       
-              if (!creep.memory.W9S7) {
+        if (creep.ticksToLive < 50) {
+            Game.spawns.Spawn6.memory.minLongDistanceBuilders = {W9S7: 2};
+        }
+        if (!creep.memory.W9S7) {
             creep.travelTo(Game.flags['W9S7']);
             if (creep.pos.isNearTo(Game.flags['W9S7'])) {
                 creep.memory.W9S7 = true;
@@ -20,6 +19,7 @@ module.exports = {
         if (creep.room.name == creep.memory.target) { 
             creep.say('building');
             roleBuilder.run(creep);
+            actionRepair.run(creep);
         }
         else { // go to target room
             var exit = creep.room.findExitTo(creep.memory.target);
