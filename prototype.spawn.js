f91f4e7f5c182b021fae810d0ba68dec81e1b166
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-08-14 10:32:06 +0100

    Update prototype.spawn.js

--- prototype.spawn.js
+++ prototype.spawn.js
@@ -47,16 +47,16 @@ Game.spawns.Spawn2.memory.minCreeps = {repairer: 0, builder: 0, lorry: 0, towerl
 
 Game.spawns.Spawn3.memory.minNumberOfreservers = {W2S8: 0};
 Game.spawns.Spawn3.memory.mincontrollerattackers = {W7S7: 0};
-Game.spawns.Spawn3.memory.minCreeps = {harvester: 1, repairer: 0, builder: 1, lorry: 1, wallRepairer: 0,
+Game.spawns.Spawn3.memory.minCreeps = {harvester: 0, repairer: 0, builder: 1, lorry: 2, wallRepairer: 0,
                                        towerlorry: 1, upgrader: 0, rampartrepairer: 0, extractor: 0, dismantler: 0};
 Game.spawns.Spawn3.memory.minLongDistanceHarvesters = {W3S7: 1};
 
 Game.spawns.Spawn4.memory.minCreeps = {harvester: 0, builder: 0, repairer: 0, upgrader: 0, dismantler: 0};
-Game.spawns.Spawn4.memory.minLongDistanceHarvesters = {W2S8: 1, W3S7: 1};
+Game.spawns.Spawn4.memory.minLongDistanceHarvesters = {W2S8: 0, W3S7: 0};
 Game.spawns.Spawn4.memory.minsmallUpgraders = {W3S8: 1};
 
 
-Game.spawns.Spawn5.memory.minCreeps = {harvester: 1, upgrader: 0, wallRepairer: 0, lorry: 2,
+Game.spawns.Spawn5.memory.minCreeps = {harvester: 0, upgrader: 0, wallRepairer: 0, lorry: 2,
 dismantler: 0, antiTransporter: 0};
 Game.spawns.Spawn5.memory.minattackers = {W4S2: 0};
 Game.spawns.Spawn5.memory.minLongDistanceHarvesters = {W1S7: 1};
@@ -87,7 +87,7 @@ Game.spawns.Spawn1.memory.mintowerdrainers3 = {W9S3: 0};
 Game.spawns.Spawn6.memory.mintowerdrainers4 = {W9S5: 0};
 Game.spawns.Spawn6.memory.mintowerdrainers5 = {W9S5: 0};
 
-Game.spawns.Spawn7.memory.minCreeps = {harvester: 0, towerlorry: 1, upgrader: 1, wallRepairer: 0, storagelorry: 0,
+Game.spawns.Spawn7.memory.minCreeps = {harvester: 0, towerlorry: 1, upgrader: 0, wallRepairer: 1, storagelorry: 0,
 				       rampartrepairer: 0, lorry: 2, builder: 0, extractor: 0, dismantler: 0};
 Game.spawns.Spawn7.memory.minLongDistanceHarvesters = {W7S7: 1, W8S6: 1};
 Game.spawns.Spawn7.memory.minNumberOfreservers = {W8S5: 0};
