bde46a6f15fcda16414917f58edab548e5d6e244
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-08-14 15:38:37 +0100

    Update link-controller.js

--- link-controller.js
+++ link-controller.js
@@ -1,25 +1,19 @@
 var linkController = {
-       
-
-    run: function(link) {
-
-      //  const upgradeLink = Game.getObjectById('60f7cd829709c9966154d996'); //remote mining link in W71N69
-        const upgradeLink2 = Game.getObjectById('60f7d1093bd3cc14ace13cfa'); //remote mining link in W62N61
-        
-           
-     //   const upgradeLinkEnergyNeeded = upgradeLink.energyCapacity - upgradeLink.energy;
+    run: function (link) {
+        const upgradeLink = Game.getObjectById('60f7cd829709c9966154d996'); 
+        const upgradeLink2 = Game.getObjectById('60f7d1093bd3cc14ace13cfa');
+        const upgradeLinkEnergyNeeded = upgradeLink.energyCapacity - upgradeLink.energy;
         const upgradeLinkEnergyNeeded2 = upgradeLink2.energyCapacity - upgradeLink2.energy;
-        
-    if (Game.time % 5 == 0) {    
-     //   if (link.energy >= upgradeLinkEnergyNeeded) {
-      //      link.transferEnergy(upgradeLink, upgradeLinkEnergyNeeded)
-       // }
-           
-        if (link.energy >= upgradeLinkEnergyNeeded2) {
-            link.transferEnergy(upgradeLink2, upgradeLinkEnergyNeeded2)
-        }
-      }
-    }
-};
 
+        if (Game.time % 2 == 0) {
+            if (link.energy >= upgradeLinkEnergyNeeded) {
+                link.transferEnergy(upgradeLink, upgradeLinkEnergyNeeded)
+            }
+
+            if (link.energy >= upgradeLinkEnergyNeeded2) {
+                link.transferEnergy(upgradeLink2, upgradeLinkEnergyNeeded2)
+            }          
+        }
+    }
+}
 module.exports = linkController;
