79afac518aad7230fa58b8a0bdc62656a4e14395
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-08-15 21:28:41 +0100

    Update link-controller.js

--- link-controller.js
+++ link-controller.js
@@ -1,28 +1,28 @@
 var linkController = {
     run: function (link) {
-        const upgradeLink = Game.getObjectById('60f7d1093bd3cc14ace13cfa'); //W7S8 centre
-        const upgradeLink2 = Game.getObjectById('60f7cd829709c9966154d996'); //W7S8 
-        const upgradeLink3 = Game.getObjectById('6102b6bd0351a81429f0d43f'); //W7S8
+        const upgradeLink1 = Game.getObjectById('60f7d1093bd3cc14ace13cfa'); //W7S8 centre
+        const upgradeLink2 = Game.getObjectById('60f7cd829709c9966154d996'); //W7S8
+       const upgradeLink3 = Game.getObjectById('6102b6bd0351a81429f0d43f'); //W7S8
         
-        const upgradeLinkEnergyNeeded = upgradeLink.energyCapacity - upgradeLink.energy;
+        const upgradeLinkEnergyNeeded1 = upgradeLink1.energyCapacity - upgradeLink1.energy;
         const upgradeLinkEnergyNeeded2 = upgradeLink2.energyCapacity - upgradeLink2.energy;
         const upgradeLinkEnergyNeeded3 = upgradeLink3.energyCapacity - upgradeLink3.energy;
 
         
-if (Game.time % 3 == 0) {  
+if (Game.time % 5 == 0) {
 
-            if (link.energy >= upgradeLinkEnergyNeeded) {
-                link.transferEnergy(upgradeLink, upgradeLinkEnergyNeeded)
+            if (link.energy >= upgradeLinkEnergyNeeded1) {
+                link.transferEnergy(upgradeLink1, upgradeLinkEnergyNeeded1)
             }
-          
-            if (link.energy >= upgradeLinkEnergyNeeded2) {
+    console.log('linked2') 
+           /* if (link.energy >= upgradeLinkEnergyNeeded2) {
                 link.transferEnergy(upgradeLink2, upgradeLinkEnergyNeeded2)
             }
-          console.log('linked2')
+         
 
             if (link.energy >= upgradeLinkEnergyNeeded3) {
                 link.transferEnergy(upgradeLink3, upgradeLinkEnergyNeeded3)
-            }
+            }*/
 }
     }
 }
