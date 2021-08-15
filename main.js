2756629536c6f3c7f26f4f84b74e6ff655edce5e
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-08-15 13:39:59 +0100

    Update main.js

--- main.js
+++ main.js
@@ -146,6 +146,8 @@ Game.spawns.Spawn6.memory.crewRoom = 'W9S5'
               linkController.run(uploadLink1);
               const uploadLink2 = Game.getObjectById('60f7cd829709c9966154d996'); // loc at controller
               linkController.run(uploadLink2);
+		const uploadLink3 = Game.getObjectById('6102b6bd0351a81429f0d43f'); // loc at controller
+		linkController.run(uploadLink3);
 
 	//const profiler = require('screeps-profiler');
 	//profiler.enable();
