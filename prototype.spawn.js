8ee385caa3380e4fc53f0f2cc679a6f420c1ab73
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-08-16 08:49:13 +0100

    Update prototype.spawn.js

--- prototype.spawn.js
+++ prototype.spawn.js
@@ -87,7 +87,7 @@ Game.spawns.Spawn1.memory.mintowerdrainers3 = {W9S3: 0};
 Game.spawns.Spawn6.memory.mintowerdrainers4 = {W9S5: 0};
 Game.spawns.Spawn6.memory.mintowerdrainers5 = {W9S5: 0};
 
-Game.spawns.Spawn7.memory.minCreeps = {harvester: 0, towerlorry: 1, upgrader: 0, wallRepairer: 0, storagelorry: 1,
+Game.spawns.Spawn7.memory.minCreeps = {harvester: 0, towerlorry: 1, upgrader: 0, wallRepairer: 0, storagelorry: 0,
 				       rampartrepairer: 0, lorry: 2, builder: 0, extractor: 0, dismantler: 0};
 Game.spawns.Spawn7.memory.minLongDistanceHarvesters = {W7S7: 1, W8S6: 0};
 Game.spawns.Spawn7.memory.minNumberOfreservers = {W8S5: 0};
