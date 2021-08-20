7ccc3c806f946a6128b7b1f453a40d6585d4dbb3
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-08-20 19:35:53 +0100

    Update link-controller.js

--- link-controller.js
+++ link-controller.js
@@ -8,12 +8,12 @@ var linkController = {
         const upgradeLinkEnergyNeeded2 = upgradeLink2.energyCapacity - upgradeLink2.energy;
         const upgradeLinkEnergyNeeded3 = upgradeLink3.energyCapacity - upgradeLink3.energy;      
             if (link.energy >= upgradeLinkEnergyNeeded1) {
-                link.transferEnergy(upgradeLink1, upgradeLinkEnergyNeeded2)
+                link.transferEnergy(upgradeLink1, upgradeLinkEnergyNeeded1)
             }
         
             
             if (link.energy >= upgradeLinkEnergyNeeded2) {
-                link.transferEnergy(upgradeLink1, upgradeLinkEnergyNeeded1)
+                link.transferEnergy(upgradeLink1, upgradeLinkEnergyNeeded2)
             }
                
         /*
