0531a254d7aeb17b678451cff1190262968ed3d5
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-08-21 16:46:05 +0100

    Update role.attacker.js

--- role.attacker.js
+++ role.attacker.js
@@ -1,73 +1,63 @@
 module.exports = {
-
-
     run: function (creep) {
         /*
-         if (creep.ticksToLive < 150) {
-
-        creep.suicide();
-        }*/
+                if (creep.ticksToLive < 150) {
+                        creep.suicide();
+                                }
+                                */
         creep.say('OMG!😨', true);
+        if (!creep.memory.W7S7 && creep.room.name === 'W7S7'|| creep.room.name === 'W7S6') {
+            creep.travelTo(Game.flags['W7S7']);
+            if (creep.pos.isNearTo(Game.flags['W7S7'])) {
+                creep.memory.W7S7 = true;
+            }
+            return;
+        }
+        if (!creep.memory.W9S5 && creep.room.name === 'W9S5'|| creep.room.name === 'W9S6') {
+            creep.travelTo(Game.flags['W9S5']);
+            if (creep.pos.isNearTo(Game.flags['W9S5'])) {
+                creep.memory.W9S5 = true;
+            }
+            return;
+        }
+        if (!creep.memory.W3S7 && creep.room.name === 'W3S7'|| creep.room.name === 'W3S8') {
+            creep.travelTo(Game.flags['W3S7']);
+            if (creep.pos.isNearTo(Game.flags['W3S7'])) {
+                creep.memory.W3S7 = true;
+            }
+            return;
+        }
+        if (!creep.memory.W1S7 && creep.room.name === 'W1S7' || creep.room.name === 'W1S8') {
+            creep.moveTo(Game.flags['W1S7']);
+            if (creep.pos.isNearTo(Game.flags['W1S7'])) {
+                creep.memory.W1S7 = true;
+            }
+            return;
+        }
+
+        if (!creep.memory.W7S5 && creep.room.name === 'W7S5' || creep.room.name === 'W9S6') {
+            creep.moveTo(Game.flags['w7s6f4']);
+            if (creep.pos.isNearTo(Game.flags['w7s6f4'])) {
+                creep.memory.W7S5 = true;
+            }
+            return;
+        }
+
+       /*
         if (!creep.memory.w1s7f1) {
             creep.travelTo(Game.flags['w1s7f1']);
             if (creep.pos.isNearTo(Game.flags['w1s7f1'])) {
                 creep.memory.w1s7f1 = true;
             }
             return;
-        }
-        if (!creep.memory.w1s5f1) {
-            creep.travelTo(Game.flags['w1s5f1']);
-            if (creep.pos.isNearTo(Game.flags['w1s5f1'])) {
-                creep.memory.w1s5f1 = true;
-            }
-            return;
-        }
-        if (!creep.memory.w2s5f1) {
-            creep.travelTo(Game.flags['w2s5f1']);
-            if (creep.pos.isNearTo(Game.flags['w2s5f1'])) {
-                creep.memory.w2s5f1 = true;
-            }
-            return;
-        }
-        if (!creep.memory.w2s4f1) {
-            creep.travelTo(Game.flags['w2s4f1']);
-            if (creep.pos.isNearTo(Game.flags['w2s4f1'])) {
-                creep.memory.w2s4f1 = true;
-            }
-            return;
-        }
-
-        if (!creep.memory.w3s4f1) {
-            creep.travelTo(Game.flags['w3s4f1']);
-            if (creep.pos.isNearTo(Game.flags['w3s4f1'])) {
-                creep.memory.w3s4f1 = true;
-            }
-            return;
-        }
-        if (!creep.memory.w4s4f1) {
-            creep.travelTo(Game.flags['w4s4f1']);
-            if (creep.pos.isNearTo(Game.flags['w4s4f1'])) {
-                creep.memory.w4s4f1 = true;
-            }
-            return;
-        }
-
-        if (!creep.memory.w4s3f1) {
-            creep.travelTo(Game.flags['w4s3f1']);
-            if (creep.pos.isNearTo(Game.flags['w4s3f1'])) {
-                creep.memory.w4s3f1 = true;
-            }
-            return;
-        }
-        if (!creep.memory.w4s2f1) {
-            creep.travelTo(Game.flags['w4s2f1']);
-            if (creep.pos.isNearTo(Game.flags['w4s2f1'])) {
-                creep.memory.w4s2f1 = true;
-            }
-            return;
-        }
-
+        }*/
         if (creep.hits > 0.9 * creep.hitsMax) {
+            let hostileStructure = creep.pos.findClosestByRange(FIND_HOSTILE_STRUCTURES);
+            if (hostileStructure) {
+                if (creep.attack(hostileStructure) === ERR_NOT_IN_RANGE) {
+                    creep.travelTo(hostileStructure);
+                }
+            }
             let target = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS, {
                 filter: c => ((c.pos.getRangeTo(creep) < 48))
             });
