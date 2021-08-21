1fe2a3cd5544f9558f00ee274801463379a843db
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-08-21 13:53:28 +0100

    Update link-controller.js

--- link-controller.js
+++ link-controller.js
@@ -4,23 +4,17 @@ var linkController = {
         const upgradeLink2 = Game.getObjectById('60f7cd829709c9966154d996'); //W7S8
        const upgradeLink3 = Game.getObjectById('6102b6bd0351a81429f0d43f'); //W7S8
         
-        const upgradeLinkEnergyNeeded1 = upgradeLink1.energyCapacity - upgradeLink1.energy;
+        const upgradeLinkEnergyNeeded1 = upgradeLink1.energyCapacity - upgradeLink1.energy*0.9;
         const upgradeLinkEnergyNeeded2 = upgradeLink2.energyCapacity - upgradeLink2.energy;
-        const upgradeLinkEnergyNeeded3 = upgradeLink3.energyCapacity - upgradeLink3.energy;      
-            if (link.energy >= upgradeLinkEnergyNeeded1) {
-                link.transferEnergy(upgradeLink1, upgradeLinkEnergyNeeded1)
+        const upgradeLinkEnergyNeeded3 = upgradeLink3.energyCapacity - upgradeLink3.energy;
+
+        if (Game.time % 50 == 0) {
+            console.log('linked2')
+            if (link.store[RESOURCE_ENERGY] >= 300) {
+                link.transferEnergy(upgradeLink1)
             }
-        
-            
-            if (link.energy >= upgradeLinkEnergyNeeded2) {
-                link.transferEnergy(upgradeLink1, upgradeLinkEnergyNeeded2)
-            }
-               
-        /*
-            if (link.energy >= upgradeLinkEnergyNeeded3) {
-                link.transferEnergy(upgradeLink3, upgradeLinkEnergyNeeded3)
-            }*/
-        
+
+        }
     }
 }
 module.exports = linkController;
