3587c692019156d11219806572507a48259503fb
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-08-23 12:34:11 +0100

    Update link-controller.js

--- link-controller.js
+++ link-controller.js
@@ -2,7 +2,7 @@ var linkController = {
     run: function (link) {
         const upgradeLink1 = Game.getObjectById('60f7d1093bd3cc14ace13cfa'); //W7S8 centre
         const upgradeLink2 = Game.getObjectById('60f7cd829709c9966154d996'); //W7S8
-       const upgradeLink3 = Game.getObjectById('6102b6bd0351a81429f0d43f'); //W7S8
+      // const upgradeLink3 = Game.getObjectById('6102b6bd0351a81429f0d43f'); //W7S8
         
       /*  const upgradeLinkEnergyNeeded1 = upgradeLink1.energyCapacity - upgradeLink1.energy*0.9;
         const upgradeLinkEnergyNeeded2 = upgradeLink2.energyCapacity - upgradeLink2.energy;
