9a16782148b3f3c333d3225996a7ff97baee947a
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-08-23 11:53:01 +0100

    Update role.longDistanceBuilder.js

--- role.longDistanceBuilder.js
+++ role.longDistanceBuilder.js
@@ -9,13 +9,24 @@ module.exports = {
     /*    if (creep.ticksToLive < 50) {
             Game.spawns.Spawn6.memory.minLongDistanceBuilders = {W9S7: 2};
         }*/
-        if (!creep.memory.W9S7) {
+     
+
+        if (!creep.memory.W7S7 && creep.room.name === 'W7S6'|| creep.room.name === 'W7S7') {
+            creep.travelTo(Game.flags['W7S7']);
+            if (creep.pos.isNearTo(Game.flags['W7S7'])) {
+                creep.memory.W7S7 = true;
+            }
+            return
+        }
+
+        if (!creep.memory.W9S7 && creep.room.name === 'W9S6'|| creep.room.name === 'W9S7') {
             creep.travelTo(Game.flags['W9S7']);
             if (creep.pos.isNearTo(Game.flags['W9S7'])) {
                 creep.memory.W9S7 = true;
             }
-            return;
-        }           
+            return
+        }
+
         if (creep.room.name == creep.memory.target) { 
             creep.say('building');
             actionRepair.run(creep);
@@ -28,4 +39,3 @@ module.exports = {
         }        
     }
 };
-
