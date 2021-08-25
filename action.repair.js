0de9968e83052f80cbd565d687677041762ca0e5
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-08-25 23:25:24 +0100

    Update action.repair.js

--- action.repair.js
+++ action.repair.js
@@ -5,7 +5,7 @@ module.exports = {
         var mineral = creep.room.find(FIND_MINERALS)[0]
         var mineralContainer =  mineral.pos.findInRange(FIND_STRUCTURES, 1, {
                filter: s => s.structureType == STRUCTURE_CONTAINER})[1];
-        if ((mineral)&&(mineralContainer)&&(mineralContainer.hits<0.9*mineralContainer.hitsMax)) { // if there is a  mineral mine (becaue there is container)
+        if ((mineral)&&(mineralContainer)&&(mineralContainer.hits<0.6*mineralContainer.hitsMax)) { // if there is a  mineral mine (becaue there is container)
             if (creep.repair(mineralContainer) == ERR_NOT_IN_RANGE) {
                 creep.travelTo(mineralContainer);
             }
@@ -18,7 +18,7 @@ module.exports = {
                     && (s.hits<0.9*s.hitsMax) })
             if (structure == undefined) {
                 structure = creep.pos.findClosestByRange(FIND_STRUCTURES, { 
-       filter: (s) => s.hits < s.hitsMax && s.structureType == STRUCTURE_ROAD})
+       filter: (s) => s.hits < 0.6*s.hitsMax && s.structureType == STRUCTURE_ROAD})
             }
             if (structure != undefined) {
                 if (creep.repair(structure) == ERR_NOT_IN_RANGE) {
