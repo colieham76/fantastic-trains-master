a92dba3a67622f73f25e3060b9e4211aaf2358b6
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-08-27 17:51:19 +0100

    Update action.build.js

--- action.build.js
+++ action.build.js
@@ -8,9 +8,9 @@ module.exports = {
         if (!constructionSite) {
             constructionSite = creep.pos.findClosestByRange(FIND_CONSTRUCTION_SITES);
         }
-        else {
-            constructionSite = constructionSite[0]
-        }
+        //else {
+        //    constructionSite = constructionSite[0]
+        //}
         if (constructionSite != undefined) {
             if (creep.build(constructionSite) == ERR_NOT_IN_RANGE) {
                 creep.travelTo(constructionSite, { maxRooms: 1 });
