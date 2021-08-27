93c870276ced3e716f5f9a9902cd7e851b6dac92
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-08-27 17:34:17 +0100

    Update role.builder.js

--- role.builder.js
+++ role.builder.js
@@ -54,16 +54,19 @@ module.exports = {
                 if (creep.build(constructionSites[0]) === ERR_NOT_IN_RANGE) {
                     creep.travelTo(constructionSites[0]);
                 }
-            } else {
-                if (creep.room.name == 'W9S7') {
-                    creep.moveTo(12, 6, {visualizePathStyle: {stroke: '#ffaa00'}});
-                }
-                if (creep.room.name == 'W7S7') {
-                    creep.moveTo(29, 40, {visualizePathStyle: {stroke: '#ffaa00'}});
-            }
-        } else {
+            } 
+            else {
             if (!creep.memory.building) {
                 (creep.getEnergy(true, true));
+            }
+                else {
+                    if (creep.room.name == 'W9S7') {
+                        creep.moveTo(12, 6, {visualizePathStyle: {stroke: '#ffaa00'}});
+                    }
+                    if (creep.room.name == 'W7S7') {
+                        creep.moveTo(29, 40, {visualizePathStyle: {stroke: '#ffaa00'}});
+                    }
+                } 
             }
         }
     }
