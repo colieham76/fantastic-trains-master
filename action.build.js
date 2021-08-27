685a4a0a39b1463608dda2144b1fcf05f0021380
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-08-27 17:59:07 +0100

    Update action.build.js

--- action.build.js
+++ action.build.js
@@ -3,12 +3,7 @@ var actionRepair = require('action.repair');
 module.exports = {
     run: function (creep) {
         const constructionSite = creep.room.find(FIND_CONSTRUCTION_SITES);
-        if (!constructionSite) {
-            constructionSite = creep.pos.findClosestByRange(FIND_CONSTRUCTION_SITES);
-        }
-       /* else {
-            constructionSite = constructionSite[0]
-        }*/
+       
         if (constructionSite != undefined) {
             if (creep.build(constructionSite) == ERR_NOT_IN_RANGE) {
                 creep.travelTo(constructionSite);
