fac6648e03a0d75b2f838aadc121ec46f90285a3
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-08-29 13:24:55 +0100

    Update action.repair.js

--- action.repair.js
+++ action.repair.js
@@ -5,7 +5,7 @@ module.exports = {
         var mineral = creep.room.find(FIND_MINERALS)[0]
         var mineralContainer =  mineral.pos.findInRange(FIND_STRUCTURES, 1, {
                filter: s => s.structureType == STRUCTURE_CONTAINER})[1];
-        if ((mineral)&&(mineralContainer)&&(mineralContainer.hits< 240000)) { // if there is a  mineral mine (becaue there is container)
+        if ((mineral)&&(mineralContainer)) { // if there is a  mineral mine (becaue there is container)
             if (creep.repair(mineralContainer) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(mineralContainer);
             }
@@ -15,10 +15,10 @@ module.exports = {
        filter: (s) => (s.structureType != STRUCTURE_ROAD
                     && s.structureType != STRUCTURE_WALL
                     && s.structureType != STRUCTURE_RAMPART)
-                    && (s.hits<0.8*s.hitsMax) })
+                    && (s.hits<0.9*s.hitsMax) })
             if (structure == undefined) {
                 structure = creep.pos.findClosestByRange(FIND_STRUCTURES, { 
-       filter: (s) => s.hits < Math.floor(0.8*s.hitsMax) && s.structureType == STRUCTURE_ROAD})
+       filter: (s) => s.hits < Math.floor(0.9*s.hitsMax) && s.structureType == STRUCTURE_ROAD})
             }
             if (structure != undefined) {
                 if (creep.repair(structure) == ERR_NOT_IN_RANGE) {
