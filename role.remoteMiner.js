626df023a02f226acbcf0cfdb4665298bdf2a657
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-08-29 12:32:17 +0100

    Update role.remoteMiner.js

--- role.remoteMiner.js
+++ role.remoteMiner.js
@@ -1,29 +1,27 @@
 const actionRepair = require('action.repair');
 module.exports = {
-	run: function (creep) {			
-		if (creep.room.name == creep.memory.target) {					
-				let source = Game.getObjectById(creep.memory.sourceId);
-				let container = source.pos.findInRange(FIND_STRUCTURES, 1, {
-					filter: (s) => (s.structureType == STRUCTURE_CONTAINER) && s.energy > 199999
-				})[0];			
-				if (container != undefined) {
-					if (creep.pos.isEqualTo(container.pos)) {
-						creep.harvest(source);
-					} else {
-						let thingUnderFeet = creep.room.lookForAt(LOOK_STRUCTURES, creep)[0];
-						if (thingUnderFeet && thingUnderFeet.structureType == STRUCTURE_CONTAINER) {
-							creep.repair(thingUnderFeet);
-						}
-						creep.moveTo(container);
-					}
+	run: function (creep) {
+		if (creep.room.name == creep.memory.target) {
+			let source = Game.getObjectById(creep.memory.sourceId);
+			let container = source.pos.findInRange(FIND_STRUCTURES, 1, {
+				filter: (s) => s.structureType == STRUCTURE_CONTAINER,
+			})[0];
+
+			if (container != undefined) {
+				if (creep.pos.isEqualTo(container.pos)) {
+					creep.harvest(source);
 				} else {
-					if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
-						creep.moveTo(source);
-					}
-				}	
-		} else {	
+					creep.moveTo(container);
+				}
+			} else {
+				if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
+					creep.moveTo(source);
+				}
+			}
+			//actionRepair.run(creep);
+		} else {
 			let exit = creep.room.findExitTo(creep.memory.target);
 			creep.moveTo(creep.pos.findClosestByRange(exit));
-		}		
+		}
 	}
 };
