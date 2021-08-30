e2c13735df6fb5141e1a1b40500ea8e2115ecab5
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-08-30 13:29:44 +0100

    Update role.attacker.js

--- role.attacker.js
+++ role.attacker.js
@@ -72,18 +72,18 @@ module.exports = {
 				    creep.travelTo(hostileStructure);
 			    }
 		    }
-		    if (creep.room.name === 'W8S6' && Game.rooms[this.room].find(FIND_RUINS).length > 0) {
-			    Game.spawns.Spawn7.memory.minattackers = {W8S6: 0, W7S7: 1}
-		    }
-		   
+		    if (creep.room.name === 'W8S6') {
+			    let ruins = creep.room.find(FIND_RUINS)			    
+			    if (ruins.length != 0) {
+				    Game.spawns.Spawn7.memory.minattackers = {W8S6: 0, W7S7: 1}
+			    }
+		    }		   
 		    if (creep.room.name === 'W7S7') {
 			    let ruins = creep.room.find(FIND_RUINS)			    
 			    if (ruins.length != 0) {
 				    Game.spawns.Spawn7.memory.minattackers = {W8S6: 1, W7S7: 0}
 			    }
-		    }
-		    
-		    
+		    }	    		    
 		    let target = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS, {
 			    filter: c => ((c.pos.getRangeTo(creep) < 48))
 		    });
