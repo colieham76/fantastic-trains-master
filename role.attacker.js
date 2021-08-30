bf9a339474f381f2e57f710d0da312b60cc31999
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-08-30 13:26:58 +0100

    Update role.attacker.js

--- role.attacker.js
+++ role.attacker.js
@@ -77,12 +77,7 @@ module.exports = {
 		    }
 		   
 		    if (creep.room.name === 'W7S7') {
-			    let ruins = this.room.find(FIND_RUINS), {
-				    filter: (r) =>
-				    (r.store.getUsedCapacity(RESOURCE_ENERGY) = 0)
-		    });
-
-			    
+			    let ruins = creep.room.find(FIND_RUINS)			    
 			    if (ruins.length != 0) {
 				    Game.spawns.Spawn7.memory.minattackers = {W8S6: 1, W7S7: 0}
 			    }
