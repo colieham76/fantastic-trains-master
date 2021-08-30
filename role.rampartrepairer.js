9a3118c0ed12af1433267fbb58c3b4c6de2b20ae
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-08-30 11:34:20 +0100

    Update role.rampartrepairer.js

--- role.rampartrepairer.js
+++ role.rampartrepairer.js
@@ -8,26 +8,35 @@ module.exports = {
             creep.memory.working = true;
             creep.say('offload');
         }
+
         if (creep.memory.working !== true) {
-            if (creep.withdraw(energy, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
-                creep.moveTo(energy);
-            }
-        }
-        else {
-            var target = Game.getObjectById(Game.rooms[creep.room.name].memory.toBuildRampart);
-            if ((runEveryTicks(20) === true)
-                || (target === undefined)) {
-                Game.rooms[creep.room.name].memory.toBuildRampart = whichRampartToBuild(creep.room).id
-            }
-            if (target != undefined) {
-                creep.say('r');
-                //console.log(creep.room.name,target);
-                if (creep.repair(target) === ERR_NOT_IN_RANGE) {                  
-                    creep.moveTo(target);
-                 //   creep.repair(target)
+            var [resourceID, ifDropped] = evaluateEnergyResources(creep, false, false,
+                true, true); // find energy function in myFunctions
+            if (resourceID !== undefined) {
+                energy = Game.getObjectById(resourceID);
+                if (ifDropped) { // if energy is dropped
+                    if (creep.pickup(energy) === ERR_NOT_IN_RANGE) {
+                        creep.travelTo(energy);
+                    }
+                } else { // energy is from container, storage or link
+                    if (creep.withdraw(energy, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+                        creep.travelTo(energy);
+                    }
                 }
             }
-            creep.getEnergy(true, false);        
+        } else {
+            var target = Game.getObjectById(Game.rooms[creep.room.name].memory.toBuildRampart);
+            if ((runEveryTicks(50) === true) || (target === undefined)) {
+                Game.rooms[creep.room.name].memory.toBuildRampart = whichRampartToBuild(creep.room).id
+            }
+            if (target !== undefined) {
+                creep.say('r');
+                //console.log(creep.room.name,target);
+                if (creep.repair(target) === ERR_NOT_IN_RANGE) {
+                    creep.moveTo(target);
+                    creep.repair(target)
+                }
+            }
         }
     }
 };
