3410c89e66fd93b66ed92c14a15fc4c16812f867
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-08-31 18:03:52 +0100

    Update role.storagelorry.js

--- role.storagelorry.js
+++ role.storagelorry.js
@@ -44,18 +44,20 @@ module.exports = {
                     creep.moveTo(storageVar);              
                 }   
             else {
-                creep.moveTo(25, 35, {visualizePathStyle: {stroke: '#ffaa00'}});
-                if (creep.pos == 25, 35) {
-                    if (creep.memory.working == false) {                   
-                        //LINK IN ROOM W9S6
-                        const upgradeContainer = Game.getObjectById('60f28c866ad7ca62d17c9522');
-                        if (upgradeContainer.energy > 0) {
-                            if (creep.withdraw(upgradeContainer, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                if (storageToFactory == undefined){
+                    creep.moveTo(25, 35, {visualizePathStyle: {stroke: '#ffaa00'}});
+                    if (creep.pos == 25, 35) {
+                        if (creep.memory.working == false) {                   
+                            //LINK IN ROOM W9S6
+                            const upgradeContainer = Game.getObjectById('60f28c866ad7ca62d17c9522');
+                            if (upgradeContainer.energy > 0) {
+                                if (creep.withdraw(upgradeContainer, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                                 creep.travelTo(upgradeContainer);
                             }
                         }
                     }                                                                          
+                }
                 }           
             }                                        
+            }
         }
-    }
