06ae15392641e961fe9ef8e7fb713923afb93353
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-09-02 14:55:44 +0100

    Update role.healer.js

--- role.healer.js
+++ role.healer.js
@@ -53,7 +53,7 @@ module.exports = {
         }
         //code for distant room
         if (!creep.memory.recycled){
-          creep.travelTo(new RoomPosition(27, 48, 'W4S6'));   
+          creep.travelTo(new RoomPosition(27, 47, 'W4S6'));   
            creep.memory.attaaaacck = true;
         let toHeal = lowestHealthInRoom(creep);
             if (toHeal.hits != toHeal.hitsMax && creep.heal(toHeal) == 0) {
