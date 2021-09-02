21dbf29d3577208bf199e58895c24e1891612d43
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-09-02 11:26:04 +0100

    Update role.healer.js

--- role.healer.js
+++ role.healer.js
@@ -5,15 +5,10 @@ module.exports = {
 /*
       if (creep.ticksToLive < 50) {
           Game.spawns.Spawn1.memory.minhealers = {W9S3: 2};
-      }  
-  */      
-    
-       /* 
-        
+      }            
 //if (creep.ticksToLive < 1500) {
 //                    creep.suicide();
 //            }
-
         
         if (!creep.memory.boosted) { // if creep is not boosted, find a lab to boost
             let labToGo;
@@ -34,53 +29,7 @@ module.exports = {
             }
         }
         else {*/
-        
-        /* //healer code for  adjacent room
-        if (creep.ticksToLive < 500) {// double back
-            creep.memory.recycled = false;
-            creep.memory.attaaaacck = false;
-            creep.travelTo(Game.flags['waypoint1']);
-            if (creep.pos.isNearTo(Game.flags['waypoint1'])) {
-                creep.memory.waypoint1 = true;
-            }
-            creep.say('need 2 renew');
-            if(creep.room.name === 'W1S8'){
-                selfRenew.run(creep);
-            }
-        }
-        else if (creep.pos.x !== rallypos.x && creep.pos.y !== rallypos.y) {
-            if (creep.ticksToLive < 1450) {
-                selfRenew.run(creep);
-            } else if (creep.ticksToLive > 1400) {
-                creep.memory.recycled = true;
-                creep.memory.waypoint1 = false;
-                //creep.memory.attack = true;
-            }
-            if (!creep.memory.waypoint1 && (creep.memory.recycled = true)
-                && (creep.memory.attaaaacck = false)) {
-                creep.travelTo(Game.flags['waypoint1']);
-                if (creep.pos.isNearTo(Game.flags['waypoint1'])) {
-                    creep.memory.waypoint1 = true;
-                }
-                return;
-            }
-        }
-        
-        
-        if (creep.memory.recycled 
-            && (creep.memory.attaaaacck = true)) {
-            creep.travelTo(new RoomPosition(17, 48, creep.memory.target));
-            creep.memory.attaaaacck = true;
-            let toHeal = lowestHealthInRoom(creep);
-            if (toHeal.hits != toHeal.hitsMax && creep.heal(toHeal) == 0) {
-                
-            } else {
-                creep.heal(toHeal);
-                creep.rangedHeal(toHeal);
-            }
-        }
-        */
-
+           
         if (!creep.memory.w3s8f3) {
             creep.travelTo(Game.flags['w3s8f3']);
             if (creep.pos.isNearTo(Game.flags['w3s8f3'])) {
@@ -88,22 +37,20 @@ module.exports = {
             }
             return;
         }
-      if (!creep.memory.w5a7f1) {
+        if (!creep.memory.w5a7f1) {
             creep.travelTo(Game.flags['w5a7f1']);
             if (creep.pos.isNearTo(Game.flags['w5a7f1'])) {
                 creep.memory.w5a7f1 = true;
             }
             return;
         }
-      if (!creep.memory.w5a7f2) {
+        if (!creep.memory.w5a7f2) {
             creep.travelTo(Game.flags['w5a7f2']);
             if (creep.pos.isNearTo(Game.flags['w5a7f2'])) {
                 creep.memory.w5a7f2 = true;
             }
             return;
-        }
-
-        
+        }        
         //code for distant room
         if (!creep.memory.recycled){
           creep.travelTo(new RoomPosition(27, 48, 'W4S6'));   
@@ -115,7 +62,6 @@ module.exports = {
                 creep.heal(toHeal);
                 creep.rangedHeal(toHeal);
             }
-        }
-        
+        }        
     }
 }
