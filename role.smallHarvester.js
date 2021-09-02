045171a3ca6331d27522fe49e00ca5ad868466f5
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-09-02 15:28:49 +0100

    Update role.smallHarvester.js

--- role.smallHarvester.js
+++ role.smallHarvester.js
@@ -2,26 +2,28 @@ module.exports = {
     // a function to run the logic for this role
     /** @param {Creep} creep */
     run: function(creep) {
-	    var factory = creep.pos.findClosestByRange(FIND_STRUCTURES, {
-		    filter: (structure) => structure.structureType == STRUCTURE_FACTORY
-	    });
-	    if (creep.store.getFreeCapacity() > 0) {
-		    var storage = creep.room.storage;
-		    if (creep.withdraw(storage,RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-			    creep.travelTo(storage);
-		    }
-	    }	
-	    else {
-		    var factory = Game.rooms['W9S6'].find(FIND_STRUCTURES, {
+	    if (factory.store.getUsedCapacity() < 30000) {
+		    var factory = creep.pos.findClosestByRange(FIND_STRUCTURES, {
 			    filter: (structure) => structure.structureType == STRUCTURE_FACTORY
-		    })[0];
-		    if (factory.store.getUsedCapacity() < 30000) {
-			    var targett = factory				 
+		    });
+		    if (creep.store.getFreeCapacity() > 0) {
+			    var storage = creep.room.storage;
+			    if (creep.withdraw(storage,RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+				    creep.travelTo(storage);
 			    }
-		    if (creep.transfer(targett,RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-			    creep.moveTo(targett);
-		    }
-	    }	       
+		    }	
+		    else {
+			    var factory = Game.rooms['W9S6'].find(FIND_STRUCTURES, {
+				    filter: (structure) => structure.structureType == STRUCTURE_FACTORY
+			    })[0];
+			    if (factory.store.getUsedCapacity() < 30000) {
+				    var targett = factory				 
+				    }
+			    if (creep.transfer(targett,RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+				    creep.moveTo(targett);
+			    }
+		    }	
+	    }
 	    if (creep.memory.working && creep.store[RESOURCE_ENERGY] === 0) {
 		    creep.memory.working = false;
 		    creep.say('🔄 harvest');
