bf2c59ca1e56739d06923c14653aa0135212d223
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-09-04 12:58:50 +0100

    Update prototype.spawn.js

--- prototype.spawn.js
+++ prototype.spawn.js
@@ -45,12 +45,12 @@ Game.spawns.Spawn2.memory.minsmallUpgraders = {W1S8: 1};
 Game.spawns.Spawn2.memory.minCreeps = {repairer: 0, builder: 0, lorry: 0, towerlorry: 1,
                                          rampartrepairer: 0};
 
-Game.spawns.Spawn3.memory.minNumberOfreservers = {W2S8: 1, W3S7: 1};
+Game.spawns.Spawn3.memory.minNumberOfreservers = {W2S8: 0, W3S7: 1};
 Game.spawns.Spawn3.memory.mincontrollerattackers = {W7S7: 0};
 Game.spawns.Spawn3.memory.minCreeps = {harvester: 0, repairer: 0, builder: 0, lorry: 1, wallRepairer: 0,
-                                       towerlorry: 1, upgrader: 0, rampartrepairer: 0, extractor: 0, dismantler: 0};
+                                       towerlorry: 1, upgrader: 2, rampartrepairer: 0, extractor: 0, dismantler: 0};
 Game.spawns.Spawn3.memory.minLongDistanceHarvesters = {W3S7: 0};
-Game.spawns.Spawn4.memory.minsmallHarvesters = {W3S8: 1};
+Game.spawns.Spawn4.memory.minsmallHarvesters = {W3S8: 0};
 
 Game.spawns.Spawn4.memory.minCreeps = {harvester: 0, builder: 0, repairer: 0, upgrader: 0, lorry: 1, dismantler: 0};
 Game.spawns.Spawn4.memory.minLongDistanceHarvesters = {W2S8: 2, W3S7: 1};
