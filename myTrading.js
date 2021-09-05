10194296888f10ad7e2b30150ad7a8782487e64e
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-09-05 13:16:19 +0100

    Update myTrading.js

--- myTrading.js
+++ myTrading.js
@@ -1,3 +1,33 @@
+/*
+let factory = room.find(FIND_MY_STRUCTURES, {
+            filter: s => s.structureType == STRUCTURE_FACTORY
+        })[0]
+
+        if (factory) {
+
+            let batteryQuota = 10000 // 10k
+
+            let batterySellOffers = Game.market.getAllOrders(order => order.type == ORDER_SELL && order.resourceType == RESOURCE_BATTERY && order.price <= avgPrice(RESOURCE_BATTERY) * 1.2 && order.amount >= (batteryQuota - terminal.store.getUsedCapacity([RESOURCE_BATTERY])))
+
+            if (terminal.store[RESOURCE_BATTERY] < batteryQuota && batterySellOffers[0]) {
+
+                //console.log("Found order for: " + RESOURCE_BATTERY + ", " + terminal.room + ", " + batterySellOffers[0]["id"] + ", " + batterySellOffers[0].amount + batterySellOffers[0].roomName)
+                //console.log(batteryQuota - terminal.store[RESOURCE_BATTERY])
+
+                let buyAmount = batteryQuota - terminal.store.getUsedCapacity([RESOURCE_BATTERY])
+                let buyCost = Game.market.calcTransactionCost(buyAmount, room.name, batterySellOffers[0].roomName)
+
+                //console.log(buyCost + "BC")
+
+                for (let i = batteryQuota; i > 0; i -= 1000) {
+
+                    Game.market.deal(batterySellOffers[0]["id"], i, room.name)
+                }
+            }
+        }
+*/
+
+
 // send minerals via terminal
 //Game.rooms['E94N17'].terminal.send('GH',12013,'E91N16','have fun!')
 
