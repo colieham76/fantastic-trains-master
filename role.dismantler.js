c9a82aa7bd4bf205efc6394fa5e3d42146cf4b57
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-09-07 11:45:50 +0100

    Update role.dismantler.js

--- role.dismantler.js
+++ role.dismantler.js
@@ -1,70 +1,35 @@
-
-  module.exports = {
+module.exports = {
     run: function(creep) {
-        creep.say('presious...');       
-    
-/*
- if (creep.ticksToLive < 1500) {
-creep.suicide();
-}
-  
-     */ 
-       if (!creep.memory.w10s8f1) {
-            creep.travelTo(Game.flags['w10s8f1']);
-            if (creep.pos.isNearTo(Game.flags['w10s8f1'])) {
-                creep.memory.w10s8f1 = true;
+        creep.say('presious...');          
+        /*
+         if (creep.ticksToLive < 1500) {
+         creep.suicide();
+         }  
+              */
+        if (!creep.memory.w3s8f3) {
+            creep.travelTo(Game.flags['w3s8f3']);
+            if (creep.pos.isNearTo(Game.flags['w3s8f3'])) {
+                creep.memory.w3s8f3 = true;
             }
             return;
         }
-      if (!creep.memory.w10s2f1) {
-            creep.travelTo(Game.flags['w10s2f1']);
-            if (creep.pos.isNearTo(Game.flags['w10s2f1'])) {
-                creep.memory.w10s2f1 = true;
+        if (!creep.memory.w5s7f1) {
+            creep.travelTo(Game.flags['w5s7f1']);
+            if (creep.pos.isNearTo(Game.flags['w5s7f1'])) {
+                creep.memory.w5s7f1 = true;
             }
             return;
         }
-      if (!creep.memory.w9s2f1) {
-            creep.travelTo(Game.flags['w9s2f1']);
-            if (creep.pos.isNearTo(Game.flags['w9s2f1'])) {
-                creep.memory.w9s2f1 = true;
+        if (!creep.memory.w5s7f2) {
+            creep.travelTo(Game.flags['w5s7f2']);
+            if (creep.pos.isNearTo(Game.flags['w5s7f2'])) {
+                creep.memory.w5s7f2 = true;
             }
             return;
         }
-/*
-//W9S5  route flag
-             if (!creep.memory.w6s6f1) {
-            creep.travelTo(Game.flags['w6s6f1']);
-            if (creep.pos.isNearTo(Game.flags['w6s6f1'])) {
-                creep.memory.w6s6f1 = true;
-            }
-            return;
-        }
-           if (!creep.memory.w6s5f1) {
-            creep.travelTo(Game.flags['w6s5f1']);
-            if (creep.pos.isNearTo(Game.flags['w6s5f1'])) {
-                creep.memory.w6s5f1 = true;
-            }
-            return;
-        }
-
-            if (!creep.memory.w6s4f1) {
-            creep.travelTo(Game.flags['w6s4f1']);
-            if (creep.pos.isNearTo(Game.flags['w6s4f1'])) {
-                creep.memory.w6s4f1 = true;
-            }
-            return;
-        }
-      if (!creep.memory.w6s3f1) {
-            creep.travelTo(Game.flags['w6s3f1']);
-            if (creep.pos.isNearTo(Game.flags['w6s3f1'])) {
-                creep.memory.w6s3f1 = true;
-            }
-            return;
-        }
-*/
-if (Game.flags['Dismantle'] != undefined) {
+        if (Game.flags['Dismantle'] != undefined) {
             let presious = getTargetByFlag('Dismantle','structure');
-            if (presious != undefined) { // if there is storage
+            if (presious != undefined) {
                 if (creep.dismantle(presious) == ERR_NOT_IN_RANGE) {
                     creep.travelTo(presious);
                 }
@@ -85,8 +50,6 @@ if (Game.flags['Dismantle'] != undefined) {
             if (core==undefined) {
                 core = creep.pos.findClosestByRange(FIND_HOSTILE_STRUCTURES, {
                     filter:c => c.structureType==STRUCTURE_TOWER});
-
-
                 if (core ==undefined) {
                     core = creep.pos.findClosestByRange(FIND_HOSTILE_STRUCTURES, {
                         filter:c => c.structureType==STRUCTURE_SPAWN
@@ -101,4 +64,3 @@ if (Game.flags['Dismantle'] != undefined) {
         }
     }       
 }
-
