c4d96e14c7282637bbdc73028ef428b55391e89b
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-09-08 15:41:38 +0100

    Update role.smallHarvester.js

--- role.smallHarvester.js
+++ role.smallHarvester.js
@@ -2,7 +2,6 @@ module.exports = {
     // a function to run the logic for this role
     /** @param {Creep} creep */
     run: function(creep) {
-	    /*
 	     var factory = Game.rooms['W9S6'].find(FIND_STRUCTURES, {
 				    filter: (structure) => structure.structureType == STRUCTURE_FACTORY
 			    })[0];
@@ -26,7 +25,7 @@ module.exports = {
 		    }	
 	    }
 	    
-	    var factory2 = Game.rooms['W3S8'].find(FIND_STRUCTURES, {
+	    var factory2 = Game.rooms['W9S6'].find(FIND_STRUCTURES, {
 				    filter: (structure) => structure.structureType == STRUCTURE_FACTORY
 			    })[0];
 	    if (factory2.store.getUsedCapacity() < 40000) {
@@ -34,21 +33,21 @@ module.exports = {
 			    filter: (structure) => structure.structureType == STRUCTURE_FACTORY
 		    });
 		    if (creep.store.getFreeCapacity() > 0) {
-			    var storage2 = creep.room.storage;
-			    if (creep.withdraw(storage2,RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-				    creep.travelTo(storage2);
+			    var storage = creep.room.storage;
+			    if (creep.withdraw(storage,RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+				    creep.travelTo(storage);
 			    }
 		    }	
 		    else {		  
 			    if (factory2.store.getUsedCapacity() < 40000) {
-				    var targett2 = factory2
+				    var targett = factory2				 
 				    }
-			    if (creep.transfer(targett2,RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-				    creep.moveTo(targett2);
+			    if (creep.transfer(targett,RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+				    creep.moveTo(targett);
 			    }
 		    }	
 	    }
-	    */
+	    
 	    if (creep.memory.working && creep.store[RESOURCE_ENERGY] === 0) {
 		    creep.memory.working = false;
 		    creep.say('🔄 harvest');
@@ -58,17 +57,18 @@ module.exports = {
 		    creep.say('offload');
 	    }        
 	    if (creep.memory.working === true) {       	    
-		    if(creep.room.storage) {
+		    if(creep.room.storage
+		       && creep.room.storage.store.getUsedCapacity() < creep.room.storage.store.getCapacity()) {
 			    var storage = creep.room.storage;
 			    for (const resourceType in creep.store) {
 				    if (creep.transfer(storage, resourceType) === ERR_NOT_IN_RANGE) {
-					    creep.moveTo(storage);   
+					    creep.moveTo(storage, {reusePath: 500});   
 				    }
 			    }                             
 		    }
 	    }      
 	    else {           
-		 /*   let tombstones = creep.room.find(FIND_TOMBSTONES, {
+		    let tombstones = creep.room.find(FIND_TOMBSTONES, {
 			    filter: c => _.sum(c.store) > 0
 		    });
 		    if (tombstones.length > 0) {
@@ -82,10 +82,10 @@ module.exports = {
 					    creep.say('RIP ');
 				    }
 			    }
-		    }	*/   
+		    }	   
 		     // if there is no storage (which could be possible after destroyed), try picking up some energy
 		    let energy = creep.pos.findClosestByRange(FIND_DROPPED_RESOURCES, {
-			    filter: (e) => (e.resourceType == RESOURCE_ENERGY)// && e.energy > 1499
+			    filter: (e) => (e.resourceType == RESOURCE_ENERGY) && e.energy > 1499
 		    });
 		    if (creep.pickup(energy) === ERR_NOT_IN_RANGE) {
 			    creep.moveTo(energy);
