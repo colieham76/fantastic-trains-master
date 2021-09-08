8d3246e8ac5551a43cc68d4e69d1a1e07d053bfb
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-09-08 11:43:50 +0100

    Update role.controllerAttacker.js

--- role.controllerAttacker.js
+++ role.controllerAttacker.js
@@ -10,6 +10,29 @@ module.exports = {
      //             creep.suicide();
      //     }
      
+
+if (!creep.memory.w3s8f3) {
+            creep.travelTo(Game.flags['w3s8f3']);
+            if (creep.pos.isNearTo(Game.flags['w3s8f3'])) {
+                creep.memory.w3s8f3 = true;
+            }
+            return;
+        }
+        if (!creep.memory.w5s7f1) {
+            creep.travelTo(Game.flags['w5s7f1']);
+            if (creep.pos.isNearTo(Game.flags['w5s7f1'])) {
+                creep.memory.w5s7f1 = true;
+            }
+            return;
+        }
+        if (!creep.memory.w5s7f2) {
+            creep.travelTo(Game.flags['w5s7f2']);
+            if (creep.pos.isNearTo(Game.flags['w5s7f2'])) {
+                creep.memory.w5s7f2 = true;
+            }
+            return;
+        }
+
         if (creep.room.name == creep.memory.target) { // if in target room
             if (creep.room.controller && !creep.room.controller.my) {
                 if (creep.attackController(creep.room.controller) == ERR_NOT_IN_RANGE) {
