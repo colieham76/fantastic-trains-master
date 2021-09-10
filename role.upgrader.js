235b57d80dc3449eec022e958825f42add319c02
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-09-10 22:12:11 +0100

    Update role.upgrader.js

--- role.upgrader.js
+++ role.upgrader.js
@@ -2,47 +2,38 @@ module.exports = {
     // a function to run the logic for this role
     /** @param {Creep} creep */
     run: function (creep) {
-        
-        
         // upgrader route flag
-/*
-        if (!creep.memory.w3s8f3) {
-            creep.travelTo(Game.flags['w3s8f3']);
-            if (creep.pos.isNearTo(Game.flags['w3s8f3'])) {
-                creep.memory.w3s8f3 = true;
+
+        if (!creep.memory.w10s8f1) {
+            creep.travelTo(Game.flags['w10s8f1']);
+            if (creep.pos.isNearTo(Game.flags['w10s8f1'])) {
+                creep.memory.w10s8f1 = true;
             }
             return;
         }
-        if (!creep.memory.w5s7f1) {
-            creep.travelTo(Game.flags['w5s7f1']);
-            if (creep.pos.isNearTo(Game.flags['w5s7f1'])) {
-                creep.memory.w5s7f1 = true;
-            }
-            return;
-        }
-        if (!creep.memory.W5S6) {
-            creep.travelTo(Game.flags['W5S6']);
-            if (creep.pos.isNearTo(Game.flags['W5S6'])) {
-                creep.memory.W5S6 = true;
+        if (!creep.memory.w10s5f1) {
+            creep.travelTo(Game.flags['w10s5f1']);
+            if (creep.pos.isNearTo(Game.flags['w10s5f1'])) {
+                creep.memory.w10s5f1 = true;
             }
             return;
         }
 
-         if (!creep.memory.W6S6) {
-            creep.travelTo(Game.flags['W6S6']);
-            if (creep.pos.isNearTo(Game.flags['W6S6'])) {
-                creep.memory.W6S6 = true;
+         if (!creep.memory.W9S5) {
+            creep.travelTo(Game.flags['W9S5']);
+            if (creep.pos.isNearTo(Game.flags['W9S5'])) {
+                creep.memory.W9S5 = true;
             }
             return;
         }
 
-         if (!creep.memory.W7S6) {
-            creep.travelTo(Game.flags['W7S6']);
-            if (creep.pos.isNearTo(Game.flags['W7S6'])) {
-                creep.memory.W7S6 = true;
+         if (!creep.memory.W9S6f1) {
+            creep.travelTo(Game.flags['W9S6f1']);
+            if (creep.pos.isNearTo(Game.flags['W9S6f1'])) {
+                creep.memory.W9S6f1 = true;
             }
             return;
-        }*/
+        }
 
         if (creep.memory.working && creep.store.energy == 0) {
             creep.memory.working = false;
