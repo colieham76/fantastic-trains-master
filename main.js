ef414ad43a7728af204a0ee9af7055dd0d303ecf
Colieham76 <38010913+colieham76@users.noreply.github.com>
2021-09-11 15:08:19 +0100

    Update main.js

--- main.js
+++ main.js
@@ -55,10 +55,7 @@ if (Game.time % 10 === 0) {
 	Game.rooms.W7S6.createConstructionSite(2, 22, STRUCTURE_RAMPART);
 	Game.rooms.W7S6.createConstructionSite(45, 35, STRUCTURE_RAMPART);
 	Game.rooms.W7S6.createConstructionSite(29, 47, STRUCTURE_RAMPART);	
-    Game.rooms.W9S6.createConstructionSite(39, 2, STRUCTURE_RAMPART);
-    Game.rooms.W9S6.createConstructionSite(41, 2, STRUCTURE_RAMPART);
-	Game.rooms.W9S6.createConstructionSite(19, 2, STRUCTURE_RAMPART);    		
-    Game.rooms.W9S6.createConstructionSite(37, 2, STRUCTURE_RAMPART);
+ 
 	// clear memory
     for (let name in Memory.creeps) {
     	if (Game.creeps[name] == undefined) {
